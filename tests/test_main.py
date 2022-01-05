30610368c26650897b416dec7dfb29bb183064f6
danylott <d.tuytuyshkin@gmail.com>
2022-01-05 09:28:26 +0200

    Create count_occurrences task

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,21 @@
-# TODO: add tests
-from app.main import hello_world
+import pytest
+
+from app.main import count_occurrences
 
 
-def test_hello_world():
-    assert hello_world() == "Hello, world!"
+@pytest.mark.parametrize(
+    "phrase,letter,count",
+    [
+        ("samsung", "a", 1),
+        ("samsung is gnusmas", "s", 5),
+        ("Samsung is gnusmas", "s", 5),
+        ("Abracadabra", "A", 5),
+        ("", "a", 0),
+        ("Samsung", "b", 0),
+    ]
+)
+def test_count_occurrences(phrase, letter, count):
+    assert count_occurrences(phrase, letter) == count, (
+        f"Function 'count_occurrences' should return {count}, "
+        f"when 'phrase'='{phrase}' and 'letter'='{letter}'"
+    )
