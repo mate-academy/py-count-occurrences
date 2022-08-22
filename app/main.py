0587d170309ae65309f84d56fa009d38154249f5
LitvinOleksandr <sanek.litvin@gmail.com>
2022-08-22 11:44:57 +0300

    delete comments

--- app/main.py
+++ app/main.py
@@ -14,9 +14,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
     """
-    # write your code here
-    # return phrase.lower().count(letter.lower()) - like video courses
-    # for my opinion
 
     phrase = phrase.lower()
     letter = letter.lower()
