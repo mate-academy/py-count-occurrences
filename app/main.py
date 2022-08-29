85ef53f646cf4ae6f62c01605bdf8a2ef784635c
Kyryll Katash <kyryllkatash@gmail.com>
2022-08-29 16:48:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,4 +2,3 @@ def count_occurrences(phrase: str, letter: str) -> int:
     param_phrase = phrase.lower()
     param_letter = letter.lower()
     return param_phrase.count(param_letter)
-    # newline
