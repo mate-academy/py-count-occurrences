ac5acbf742477cefc8dc7753212b43f22b7bdef6
Sergii Kantor <sergiy.kantor@gmail.com>
2022-09-08 17:39:53 +0300

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,13 +19,3 @@ def test_count_occurrences(phrase, letter, count):
         f"Function 'count_occurrences' should return {count}, "
         f"when 'phrase'='{phrase}' and 'letter'='{letter}'"
     )
-
-    
-def test_removed_comment():
-import app
-with open(app.main.__file__, "r") as f:
-    file_content = f.read()
-    comment = re.compile("# write your code here")
-    assert not comment.search(
-        file_content
-    ), "You have to remove the unnecessary comment '# write your code here'"
