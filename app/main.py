e9989bdc3fa5cbd856cc7939356f3f10f1d965b9
Евгений Сацкий <evgenijsackij@MacBook-Air-som-tillhor-Evgenij.local>
2022-09-11 09:14:53 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -14,8 +14,9 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
     """
-    c = 0
-    for i in phrase:
-        if i.lower() == letter.lower():
-            c += 1
-    return c
+    count = 0
+
+    for el in phrase:
+        if el.lower() == letter.lower():
+            count += 1
+    return count
