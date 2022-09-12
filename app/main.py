18786bf8c3ce1e7ff4ab65cbc764d6c7bc5ec2b9
Dima Ovcharenko <kukuku381@gmail.com>
2022-09-12 14:41:08 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -16,8 +16,8 @@ def count_occurrences(phrase: str, letter: str) -> int:
     """
     count = 0
 
-    for i in phrase:
-        if i.lower() == letter.lower():
+    for character in phrase:
+        if character.lower() == letter.lower():
             count += 1
 
     return count
