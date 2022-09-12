214ef5ba7fd8b141f9fc69a3821ce6f0cffbf90c
igor kosianenko <funkcionalkos@gmail.com>
2022-09-12 10:55:45 +0300

    complited

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+# from readline import parse_and_bind
+
+
 def count_occurrences(phrase: str, letter: str) -> int:
     """
     Implement count_occurrences function:
@@ -14,4 +17,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
     """
-    # write your code here
+    return phrase.lower().count(letter.lower())
