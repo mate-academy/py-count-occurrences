3462d2c1c45c6f39b4d3506ef581750636f7218c
Anna Blahitka <anettaabl@gmail.com>
2022-11-19 16:52:18 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,19 +1,44 @@
-def count_occurrences(phrase: str, letter: str) -> int:
-    """
-    Implement count_occurrences function:
+class Car:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
-    It takes a phrase and a letter and calculates the number of times
-    the letter appears in the phrase. The function is case insensitive.
 
-    count_occurrences("letter", "t") == 2
-    count_occurrences("abc", "a") == 1
-    count_occurrences("abc", "d") == 0
-    count_occurrences("ABC", "a") == 1
+class CarWashStation:
+    def __int__(self, distance_from_city_center: int,
+                clean_power: int,
+                average_rating: float,
+                count_of_ratings: int
+                ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
 
-    :param phrase: phrase to count in it
-    :param letter: letter to find occurrences of it
-    :return: count occurrences of letter in phrase
-    """
-    phrase_lower = phrase.lower()
-    counter = phrase_lower.count(letter.lower())
-    return counter
+
+def serve_cars(car: Car, wash_station: CarWashStation) -> None:
+    if Car(car.clean_mark) < CarWashStation(wash_station.clean_power):
+        return CarWashStation(wash_station.average_rating.round)
+
+
+def calculate_washing_price(car: Car, wash_station: CarWashStation) -> None:
+    price = Car(car.comfort_class) *\
+        (CarWashStation(wash_station.clean_power) - Car(car.clean_mark)) *\
+        CarWashStation(wash_station.average_rating) /\
+        CarWashStation(wash_station.distance_from_city_center)
+    return price.round
+
+
+def wash_single_car(car: Car, wash_station: CarWashStation) -> None:
+    if CarWashStation(wash_station.clean_power) > Car(car.clean_mark):
+        Car(car.clean_mark) == CarWashStation(wash_station.clean_power)
+
+
+def rate_service(wash_station: CarWashStation) -> None:
+    for i in range:
+        CarWashStation(wash_station.average_rating) + [i]
+        CarWashStation(wash_station.count_of_ratings) + [i]
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,44 @@
-import functools
+class Car:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
-def cache(func):
-    func_result = {}
+class CarWashStation:
+    def __int__(self, distance_from_city_center: int,
+                clean_power: int,
+                average_rating: float,
+                count_of_ratings: int
+                ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
 
-    @functools.wraps(func)
-    def inner(*args):
-        if f"{args}" in func_result:
-            print("Getting from cache")
-        else:
-            func_result[f"{args}"] = func(*args)
-            print("Calculating new result")
-        return func_result[f"{args}"]
-    return inner
+
+def serve_cars(car: Car, wash_station: CarWashStation) -> None:
+    if Car(car.clean_mark) < CarWashStation(wash_station.clean_power):
+        return CarWashStation(wash_station.average_rating.round)
+
+
+def calculate_washing_price(car: Car, wash_station: CarWashStation) -> None:
+    price = Car(car.comfort_class) *\
+        (CarWashStation(wash_station.clean_power) - Car(car.clean_mark)) *\
+        CarWashStation(wash_station.average_rating) /\
+        CarWashStation(wash_station.distance_from_city_center)
+    return price.round
+
+
+def wash_single_car(car: Car, wash_station: CarWashStation) -> None:
+    if CarWashStation(wash_station.clean_power) > Car(car.clean_mark):
+        Car(car.clean_mark) == CarWashStation(wash_station.clean_power)
+
+
+def rate_service(wash_station: CarWashStation) -> None:
+    for i in range:
+        CarWashStation(wash_station.average_rating) + [i]
+        CarWashStation(wash_station.count_of_ratings) + [i]
