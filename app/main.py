3b98fb28a1dfdcf48dfbba6c4d4c2ede7d1b54d8
Anna Blahitka <anettaabl@gmail.com>
2022-11-23 10:58:12 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,44 +1,32 @@
 class Car:
     def __init__(self, comfort_class: int,
                  clean_mark: int,
-                 brand: str
-                 ) -> None:
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __int__(self, distance_from_city_center: int,
-                clean_power: int,
-                average_rating: float,
-                count_of_ratings: int
-                ) -> None:
+    def __init__(self, distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
+    def serve_cars(self, cars: list) -> float:
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                return self.calculate_washing_price(cars)
 
-def serve_cars(car: Car, wash_station: CarWashStation) -> None:
-    if Car(car.clean_mark) < CarWashStation(wash_station.clean_power):
-        return CarWashStation(wash_station.average_rating.round)
+    def calculate_washing_price(self, cars: Car) -> float:
+        return cars.comfort_class * (self.clean_power - cars.clean_mark) *\
+            self.count_of_ratings / self.distance_from_city_center
 
-
-def calculate_washing_price(car: Car, wash_station: CarWashStation) -> None:
-    price = Car(car.comfort_class) *\
-        (CarWashStation(wash_station.clean_power) - Car(car.clean_mark)) *\
-        CarWashStation(wash_station.average_rating) /\
-        CarWashStation(wash_station.distance_from_city_center)
-    return price.round
-
-
-def wash_single_car(car: Car, wash_station: CarWashStation) -> None:
-    if CarWashStation(wash_station.clean_power) > Car(car.clean_mark):
-        Car(car.clean_mark) == CarWashStation(wash_station.clean_power)
-
-
-def rate_service(wash_station: CarWashStation) -> None:
-    for i in range:
-        CarWashStation(wash_station.average_rating) + [i]
-        CarWashStation(wash_station.count_of_ratings) + [i]
+    def wash_single_car(self, cars: Car) -> float:
+        if self.clean_power > cars.clean_mark:
+            cars.clean_mark = self.clean_power
+            return cars
