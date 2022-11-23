f03e1090f7263f615a957fa22ad951e134a44af9
Anna Blahitka <anettaabl@gmail.com>
2022-11-23 15:11:39 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,32 +1,19 @@
-class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
-        self.comfort_class = comfort_class
-        self.clean_mark = clean_mark
-        self.brand = brand
+def count_occurrence(phrase: str, letter: str) -> int:
+    """
+    Implement count_occurrences function:
 
+    it takes a phrase and a letter and calculates the number of times
+    the letter appears in the phrase. The function is case insensitive.
 
-class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
-        self.distance_from_city_center = distance_from_city_center
-        self.clean_power = clean_power
-        self.average_rating = average_rating
-        self.count_of_ratings = count_of_ratings
+    count_occurrence("letter", "t") == 2
+    count_occurrence("abc", "a") == 1
+    count_occurrence("abc", "d") == 0
+    count_occurrence("ABC", "a") == 1
 
-    def serve_cars(self, cars: list) -> float:
-        for car in cars:
-            if car.clean_mark < self.clean_power:
-                return self.calculate_washing_price(cars)
-
-    def calculate_washing_price(self, cars: Car) -> float:
-        return cars.comfort_class * (self.clean_power - cars.clean_mark) *\
-            self.count_of_ratings / self.distance_from_city_center
-
-    def wash_single_car(self, cars: Car) -> float:
-        if self.clean_power > cars.clean_mark:
-            cars.clean_mark = self.clean_power
-            return cars
+    :param phrase: phrase to count in it
+    :param letter: letter to find occurrences of it
+    :return: count occurrences of letter in phrase
+    """
+    phrase_lower = phrase.lower()
+    counter = phrase_lower.count(letter.lower())
+    return counter
---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+import pytest
+
+from app.main import count_occurrence
+
+
+@pytest.mark.parametrize(
+    "phrase,letter,count",
+    [
+        ("samsung", "a", 1),
+        ("samsung is gnusmas", "s", 5),
+        ("Samsung is gnusmas", "s", 5),
+        ("Abracadabra", "A", 5),
+        ("", "a", 0),
+        ("Samsung", "b", 0),
+    ]
+)
+def test_count_occurrences(phrase, letter, count):
+    assert  count_occurrence(phrase, letter) == count, (
+        f"Function 'count_occurrences' should return {count}, "
+        f"when 'phrase'='{phrase}' and 'letter'='{letter}'"
+    )
