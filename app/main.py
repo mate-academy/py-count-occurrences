4a3ec62334a3e1c802d268b7817a73797ab79dca
Arina Oltarzhevska <arinaoltarzhevska.ua@gmail.com>
2022-12-30 02:39:45 +0100

    count occurrences of letter in phrase

--- app/main.py
+++ app/main.py
@@ -16,3 +16,10 @@ def count_occurrences(phrase: str, letter: str) -> int:
     """
     # write your code here
 
+    count = 0
+
+    for char in phrase:
+        if char.upper() == letter.upper():
+            count += 1
+
+    return count
