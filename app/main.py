da83ba01baed4dfaf79c6bb65592b6a85d0e2fc4
Anastasiia Sukhopleshchenko <ann.suhopleschenko@gmail.com>
2023-06-13 23:11:06 +0300

    Fixed a name of char to more descriptive one

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :return: count occurrences of letter in phrase
     """
     count = 0
-    for i in phrase:
-        if i.lower() == letter.lower():
+    for char in phrase:
+        if char.lower() == letter.lower():
             count += 1
     return count
