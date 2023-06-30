729d44598dfbac69d012222d0431bd5765f7486b
Anton <tkacuk2291@gmail.com>
2023-07-01 01:53:01 +0300

    successfully completed task py-count-occurrences

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     Implement count_occurrences function:
 
     It takes a phrase and a letter and calculates the number of times
-    the letter appears in the phrase. The function is case insensitive.
+    the letter appears in the phrase. The function is case-insensitive.
 
     count_occurrences("letter", "t") == 2
     count_occurrences("abc", "a") == 1
@@ -19,5 +19,3 @@ def count_occurrences(phrase: str, letter: str) -> int:
         if phrase[i].lower() == letter.lower():
             count += 1
     return count
-
-
