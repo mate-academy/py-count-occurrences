ac062abf51042230ba0289cd068fe7a431650876
Anton <tkacuk2291@gmail.com>
2023-07-01 15:40:12 +0300

    successfully completed task py-count-occurrences(edited solutions)

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param phrase: phrase to count in it
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
-    """
+    """""
     count = 0
     for i in phrase:
         if i.lower() == letter.lower():
