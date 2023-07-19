20805771318a7bb2adba8b47c342579501753224
Liftoff <vladislav70707@gmail.com>
2023-07-19 08:34:53 +0200

    'test_comit_19J23'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    """test commit"""
     count_letter = phrase.lower().count(letter.lower())
+
+    """
+    or this ???
+    git checkout -b develop
+
+    return count_letter
+    """
     return count_letter
