3039368d4a88723111a6726d63b7d15fd46f28a8
Seniv Volodymyr <01156240@pw.edu.pl>
2023-07-20 19:06:36 +0200

    py_cache was deleted and the ternary operator was applied

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    if letter.isupper():
-        phrase = phrase.upper()
-    else:
-        phrase = phrase.lower()
+    # if letter.isupper():
+    #     phrase = phrase.upper()
+    # else:
+    #     phrase = phrase.lower()
+    # return phrase.count(letter)
+    phrase = phrase.upper() if letter.isupper() else phrase.lower()
     return phrase.count(letter)
