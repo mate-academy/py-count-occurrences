62314f9a51eacfef17ccd6c2c906b66e6cfadd7d
CaCtUs1k <denischernish19012004@gmail.com>
2023-07-31 10:35:22 +0300

    Better naming

--- app/main.py
+++ app/main.py
@@ -14,6 +14,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
     """
-    phrase = phrase.lower()
-    letter = letter.lower()
-    return phrase.count(letter)
+    phrase_lower = phrase.lower()
+    letter_lower = letter.lower()
+    return phrase_lower.count(letter_lower)
