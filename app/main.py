4f046f03fe556494d9145c80309f0da5195a46cf
Vasyl Dmytruk <vasyldmytruk104@gmail.com>
2023-07-31 11:52:09 +0300

    count_occurrences resolution

--- app/main.py
+++ app/main.py
@@ -14,12 +14,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
     """
-    count = 0
 
-    for char in phrase:
-        if char.lower() == letter.lower():
-            count += 1
-    return count
-# new commit
-# forget about tests_v2
-# commit from Pycharm and all checks
+    return phrase.lower().count(letter.lower())
+
