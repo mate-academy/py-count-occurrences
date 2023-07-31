fcfe309460fc57bc39f40a77c94ce934012d30fe
ORENYT <106102798+ORENYT@users.noreply.github.com>
2023-07-31 10:35:14 +0300

    Update main.py
    fixed variable name

--- app/main.py
+++ app/main.py
@@ -16,4 +16,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     """
     phrase_lower = phrase.lower()
     letter_lower = letter.lower()
-    return phrase_lowe.count(letter_lower)
+    return phrase_lower.count(letter_lower)
