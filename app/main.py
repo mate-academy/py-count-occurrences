f835e812c2e4ea9e17a2ac190a7234a2c11d6c70
mishasanin <mishasanin2020@gmail.com>
2023-08-06 17:56:32 +0300

    Implemented_test py-count

--- app/main.py
+++ app/main.py
@@ -3,7 +3,17 @@ b = []
 c = "Hi!"
 d = [1, 2]
 
+def count_occurrences(phrase: str, letter: str) -> int:
+    count = 0
+
+    for char in phrase:
+        if char.lower() == letter.lower():
+            count += 1
+
+    return count
+
 sorted_variables = {
     "mutable": [b, d],
     "immutable": [a, c]
 }
+
