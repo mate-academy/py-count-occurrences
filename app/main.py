af8c26917aec9ea0856cd1eeafdad1baef2f789a
mishasanin <mishasanin2020@gmail.com>
2023-08-06 15:56:25 +0300

    Implemented_test py-count

--- app/main.py
+++ app/main.py
@@ -1,23 +1,9 @@
-def count_occurrences(phrase: str, letter: str) -> int:
-    """
-    Implement count_occurrences function:
+a = 123
+b = []
+c = "Hi!"
+d = [1, 2]
 
-    It takes a phrase and a letter and calculates the number of times
-    the letter appears in the phrase. The function is case insensitive.
-
-    count_occurrences("letter", "t") == 2
-    count_occurrences("abc", "a") == 1
-    count_occurrences("abc", "d") == 0
-    count_occurrences("ABC", "a") == 1
-
-    :param phrase: phrase to count in it
-    :param letter: letter to find occurrences of it
-    :return: count occurrences of letter in phrase
-    """
-    count = 0
-
-    for char in phrase:
-        if char.lower() == letter.lower():
-            count += 1
-
-    return count
+sorted_variables = {
+    "mutable": [b, d],
+    "immutable": [a, c]
+}
