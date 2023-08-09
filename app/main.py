4d9fff18f7e918c19f6d45ab8d3264a4ac821f1a
Kostiantin Pastukh <kostyapastukh@gmail.com>
2023-08-09 18:23:29 +0300

    Merge pull request #178 from mate-academy/add-inspect
    comment and fix variable

--- app/main.py
+++ app/main.py
@@ -1,23 +1,24 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    """
-    Implement count_occurrences function:
+    # """
+    # Implement count_occurrences function:
+    #
+    # It takes a phrase and a letter and calculates the number of times
+    # the letter appears in the phrase. The function is case insensitive.
+    #
+    # count_occurrences("letter", "t") == 2
+    # count_occurrences("abc", "a") == 1
+    # count_occurrences("abc", "d") == 0
+    # count_occurrences("ABC", "a") == 1
+    #
+    # :param phrase: phrase to count in it
+    # :param letter: letter to find occurrences of it
+    # :return: count occurrences of letter in phrase
+    # """
 
-    It takes a phrase and a letter and calculates the number of times
-    the letter appears in the phrase. The function is case insensitive.
-
-    count_occurrences("letter", "t") == 2
-    count_occurrences("abc", "a") == 1
-    count_occurrences("abc", "d") == 0
-    count_occurrences("ABC", "a") == 1
-
-    :param phrase: phrase to count in it
-    :param letter: letter to find occurrences of it
-    :return: count occurrences of letter in phrase
-    """
     counte = 0
 
     for character in phrase:
-        if letter.lower() in phrase.lower():
+        if letter.lower() in character.lower():
             counte += 1
 
     return counte
