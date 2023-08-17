09d82597dece1461cf424818281b6bfe8d9bd216
STAS BEZDITKO <kervid2023@gmail.com>
2023-08-17 10:24:53 +0300

    fix new live and character

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     """
     count = 0
 
-    for i in phrase:
-        if i.lower() == letter.lower():
+    for character in phrase:
+        if character.lower() == letter.lower():
             count += 1
     return count
