8d2151586718339440f0071839953473706e37c4
ruslan-sketch <118385742+ruslan-sketch@users.noreply.github.com>
2023-08-19 11:48:48 +0300

    code fix (remove extra lines of code)

--- app/main.py
+++ app/main.py
@@ -1,15 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
 
-
     return phrase.count(letter)
+
 
 # Test examples
 assert count_occurrences("letter", "t") == 2
 assert count_occurrences("kitten", "t") == 2
 assert count_occurrences("Happy New Year!", "p") == 2
-
-
-
-
-
-
