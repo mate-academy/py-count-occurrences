f3bd3d6bde6f455990719b18418fda7360af3a28
ruslan-sketch <118385742+ruslan-sketch@users.noreply.github.com>
2023-08-19 11:27:26 +0300

    code fix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+
+
     return phrase.count(letter)
 
 # Test examples
-assert count_occurrences('letter', 't') == 2
-assert count_occurrences('kitten', 't') == 2
-assert count_occurrences('Happy New Year!', 'p') == 2
+assert count_occurrences("letter", "t") == 2
+assert count_occurrences("kitten", "t") == 2
+assert count_occurrences("Happy New Year!", "p") == 2
+
+
+
+
+
+
