5de7cc70fee21b81e00d5a68bd166f850b902f6e
Anton <tkacuk2291@gmail.com>
2023-09-27 17:31:35 +0300

    Solxz

--- app/main.py
+++ app/main.py
@@ -15,4 +15,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :return: count occurrences of letter in phrase
     """""
 
-    return sum(1 for i in phrase if i.lower() == letter.lower())
+    return sum(1 for character in phrase if character.lower() == letter.lower())
