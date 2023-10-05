fece05806676eaef83a9b1ea8e4c6b28ea7fbae2
Olga <olgahrytsiuk777@gmail.com>
2023-10-05 20:36:56 +0300

    New one line

--- app/main.py
+++ app/main.py
@@ -14,6 +14,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
     """
-    count = sum(1 for char in phrase if char.lower() == letter.lower())
-
-    return count
+    return phrase.lower().count(letter.lower())
