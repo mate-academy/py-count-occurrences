7274977e096c669269d9d4a0a99224c1a9e5813c
AbsoluteProgramist <108784501+maksimpro001@users.noreply.github.com>
2023-10-21 21:29:03 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    count = 0
-    for chart in phrase:
-        if chart.lower() == letter.lower():
-            count += 1
-    return count
+    return phrase.lower().count(letter.lower())
