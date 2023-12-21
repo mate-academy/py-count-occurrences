b14457d36a891281ac2a0eec12794b81bb58bd48
ladmael <don.sosiska339@gmail.com>
2023-12-21 15:11:35 +0200

    optimized

--- app/main.py
+++ app/main.py
@@ -1,6 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    result = 0
-    for symb in phrase:
-        if symb.lower() == letter.lower():
-            result += 1
-    return result
+    return phrase.lower().count(letter.lower())
