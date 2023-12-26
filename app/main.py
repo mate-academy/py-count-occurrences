c373633b27629f1febf1a79f3f4471f9e0cd9c28
Sergei Molodchik <seregavikingeda@gmail.com>
2023-12-26 23:19:22 +0200

    finich

--- app/main.py
+++ app/main.py
@@ -14,4 +14,5 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
     """
-    return (f"{phrase}, {letter} == {phrase.count(letter)}")
+    return phrase.lower().count(letter.lower())
+
