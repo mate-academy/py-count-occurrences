e6f882f1cde29da1db0a28c295db55052ede0c21
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2023-12-29 00:15:50 +0200

    code in one line

--- app/main.py
+++ app/main.py
@@ -1,18 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-<<<<<<< HEAD
     return phrase.lower().count(letter.lower())
-=======
-    # Перетворюємо обидва вхідні аргументи у нижній регістр для порівняння
-    phrase = phrase.lower()
-    letter = letter.lower()
-
-    # Лічильник для зберігання кількості входжень літери
-    count = 0
-
-    # Перебираємо кожен символ у фразі
-    for char in phrase:
-        if char == letter:
-            count += 1
-
-    return count
->>>>>>> 6dbf57b419b7ab12c48908661e5e83e77adf15fb
