7b56cef162855b19f1cef5563c34cda193736766
mkuchuman <mkuchuman1@gmail.com>
2024-01-14 16:19:42 +0100

    Rewrited in one string

--- app/main.py
+++ app/main.py
@@ -1,8 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    count = 0
-    phrase = phrase.lower()
-    letter = letter.lower()
-    for i in phrase:
-        if i == letter:
-            count += 1
-    return count
+    return phrase.lower().count(letter.lower())
