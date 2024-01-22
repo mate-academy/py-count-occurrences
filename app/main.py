5c81295551c7d1a7cd78386df3daa8e960f3d5bf
Andriy Shafran <shafran.andriy@gmail.com>
2024-01-22 19:48:56 +0200

    Fixed function using method count of string

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ def count_occurrences(phrase: str, letter: str) -> int:
     count_occurrences("abc", "d") == 0
     count_occurrences("ABC", "a") == 1
 
+    
+    
     :param phrase: phrase to count in it
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
