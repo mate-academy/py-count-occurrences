8d9d6bd662110a5f44edc1a52d8d3dd82e131ab8
Andriy Shafran <shafran.andriy@gmail.com>
2024-01-22 21:27:50 +0200

    fixed whitespace in line 11

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     count_occurrences("letter", "t") == 2
     count_occurrences("abc", "a") == 1
     count_occurrences("abc", "d") == 0
-    count_occurrences("ABC", "a") == 1 
+    count_occurrences("ABC", "a") == 1
     :param phrase: phrase to count in it
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
