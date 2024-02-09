25fc13038ee0f5c669c17dda9d69a5e7079f2eb6
twonumberfortyfives <118538621+twonumberfortyfives@users.noreply.github.com>
2024-02-09 04:34:54 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    phrase = phrase.lower()
-    letter = letter.lower()
-    count = 0
-    for i in phrase:
-        if i in letter:
-            count += 1
-    return count
+    eturn phrase.lower().count(letter.lower())
