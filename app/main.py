bace4dbc83ebb93e7343ce8b3940f211ca20f4e9
shcherban <151711805+SHCHERBANV@users.noreply.github.com>
2024-02-15 10:23:54 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    count = 0
     phrase = phrase.lower()
     letter = letter.lower()
-    for char in phrase:
-        if char == letter:
-            count += 1
+    return phrase.count(letter)
     return count
