fb3671f3458772555cadb5055195ec17b9bbc4d1
SHCHERBANV <shcherbanvasyl@gmail.com>
2024-02-25 14:52:56 -0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,4 @@
-from collections import Counter
-
-
 def count_occurrences(phrase: str, letter: str) -> int:
     phrase = phrase.lower()
-    letter = letter.lower()
-    count = Counter(phrase)[letter]
+    count = phrase.count(letter.lower())
     return count
