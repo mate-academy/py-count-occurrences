3a7e982203a37ddd06e44b5e7d35abe00eabfde3
Vitalii Tsukanov <vitalii.tsunakov@icloud.com>
2024-02-27 21:44:44 +0100

    solved task

--- app/main.py
+++ app/main.py
@@ -11,7 +11,12 @@ def count_occurrences(phrase: str, letter: str) -> int:
     count_occurrences("ABC", "a") == 1
 
     :param phrase: phrase to count in it
-    :param letter: letter to find occurrences of it
-    :return: count occurrences of letter in phrase
+    :param letter: letter to find occurrences of
+
     """
-    # write your code here
+
+    counter = 0
+    for char in phrase:
+        if char.lower() == letter.lower():
+            counter += 1
+    return counter
