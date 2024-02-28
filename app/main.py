fce59ce59f3295f62cd48f88946363e80d8bbcd2
Vitalii Tsukanov <vitalii.tsunakov@icloud.com>
2024-02-28 12:55:18 +0100

    one line

--- app/main.py
+++ app/main.py
@@ -15,8 +15,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
 
     """
 
-    counter = 0
-    for char in phrase:
-        if char.lower() == letter.lower():
-            counter += 1
-    return counter
+    return sum(1 for char in phrase if char.lower() == letter.lower())
