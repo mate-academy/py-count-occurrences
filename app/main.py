dee7d86c06c17e171671890ff4c859f163839544
Vitalii Tsukanov <vitalii.tsunakov@icloud.com>
2024-03-01 18:27:45 +0100

    remove comment

--- app/main.py
+++ app/main.py
@@ -15,6 +15,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
 
     """
 
-    #return sum(1 for char in phrase if char.lower() == letter.lower())
+
     return phrase.lower().count(letter.lower())
 
