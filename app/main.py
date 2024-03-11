61275a16d832a04da14e977d578e0499d3977e0e
Liliia <furman06228@gmail.com>
2024-03-11 13:15:59 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -15,3 +15,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :return: count occurrences of letter in phrase
     """
     # write your code here
+    return sum(1 for char in phase if char.lower() == letter.lower())
