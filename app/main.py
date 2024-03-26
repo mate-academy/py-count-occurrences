c76d39ba0868fe93bfe1b508e6fc607928ce94e2
Iryna <your@mail.com>
2024-03-26 20:06:45 +0100

    implement py-count-occurrences new test

--- app/main.py
+++ app/main.py
@@ -7,3 +7,20 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :return: The count of occurrences of the letter in the phrase.
     """
     return sum(1 for char in phrase if char.lower() == letter.lower())
+# Создаем несколько переменных разных типов
+
+
+a = 123
+b = []
+c = "Hi!"
+d = [1, 2]
+
+# Создаем словарь для сортировки переменных по их типу
+sorted_variables = {
+    "mutable": [],      # Для изменяемых (mutable) переменных
+    "immutable": []     # Для неизменяемых (immutable) переменных
+}
+
+
+def count_letter(phrase, letter):
+    return sum(1 for char in phrase if char.lower() == letter.lower())
