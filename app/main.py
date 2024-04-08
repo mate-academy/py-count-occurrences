8f1af52e009646deadde95dc7409fa06833a6b79
Triukhan <tryukhan.d@gmail.com>
2024-04-08 19:22:53 +0300

    triukhan solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,2 @@
-a = 12345678901111222
-b = 12345678901111222
-
-print(id(a), id(b))
-print(a is b)
+def count_occurrences(phrase: str, letter: str) -> int:
+    return phrase.lower().count(letter.lower())
