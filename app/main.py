5b84068b440126d3671fa3c5793ba8a2f66a6bf1
Yaroslav Khyl <yaroslav_khyl@ukr.netgit config --global user.email yaroslav_khyl@ukr.net>
2024-04-11 21:52:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for i in phrase:
-        if i.lower() == letter.lower():
-            counter += 1
-    return counter
+    return phrase.lower().count(letter.lower())
+
