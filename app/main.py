33d7aee70cd7f5b3b2da43fda3c9323be4bc723f
TkachukTara <zaz42988@gmail.com>
2024-05-13 11:18:41 +0300

    a ready-made solution for the task, V2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     count = 0
-    for lets in phrase:
-        if lets.upper() == letter.upper():
+    for char in phrase:
+        if char.upper() == letter.upper():
             count += 1
     return count
