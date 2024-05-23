ef2f17da1c467b5a7def13fe02a04e0368be2498
Serhiy <sergeynechaenko@ukr.net>
2024-05-23 21:05:36 +0300

    fix newline and character

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     """
     counter = 0
     for character in phrase:
-        if char.lower() == letter.lower():
+        if character.lower() == letter.lower():
             counter += 1
 
     return counter
