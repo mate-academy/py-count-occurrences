04b32b570cbe46637a78535ae02db02f49aa73d3
user <graban2253@gmail.com>
2024-06-12 15:00:25 +0300

    Finished

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     """
     Implement count_occurrences function:
-
     It takes a phrase and a letter and calculates the number of times
     the letter appears in the phrase. The function is case insensitive.
 
