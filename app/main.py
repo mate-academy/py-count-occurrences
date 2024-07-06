a7eca2bebe8c1cf879673f6e6e8ff5298d692078
Oleksandr Kozlov <aleksandrkozlov2015@gmail.com>
2024-07-06 17:35:15 +0200

    code without for loop and using count()

--- app/main.py
+++ app/main.py
@@ -15,4 +15,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :return: count occurrences of letter in phrase
     """
 
-    return sum(1 for char in phrase if char.lower() == letter.lower())
+    return phrase.lower().count(letter.lower())
