1506373848f169a959efd9cc568261eaee9e0b01
Ruslan Toister <rtoister.cv@gmail.com>
2024-07-15 16:27:48 +0300

    'Solution_one_line_and_use_count'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    return sum(1 for character in phrase
-               if character.lower() == letter.lower())
+    return phrase.lower().count(letter.lower())
