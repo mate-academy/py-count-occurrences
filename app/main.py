535fcd9a00f5b6740eb9dfeaba47d2a3bc4cd0b7
Mike Shmygel <m.shmygel@gmail.com>
2024-08-09 14:41:08 +0300

    fix new line

--- app/main.py
+++ app/main.py
@@ -18,4 +18,5 @@ def count_occurrences(phrase: str, letter: str) -> int:
     for character in phrase:
         if character.lower() == letter.lower():
             counter += 1
+
     return counter
