1dc81042cd26f2190a06312d50323c4477b2ecb2
Kuzmakin <kuzmakin@gmail.com>
2024-08-19 19:50:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,15 +8,11 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :return: The number of occurrences of the letter in the phrase.
     """
     number_of_letter = 0
-    # Перетворення фрази та букви в нижній регістр
     phrase = phrase.lower()
     letter = letter.lower()
-
-    # Перевірка на те, що letter є однією буквою
     if len(letter) != 1:
         raise ValueError("The 'letter' parameter should be a single character.")
 
-    # Підрахунок кількості входжень букви у фразу
     for char in phrase:
         if char == letter:
             number_of_letter += 1
@@ -24,14 +20,11 @@ def count_occurrences(phrase: str, letter: str) -> int:
     return number_of_letter
 
 
-# Отримання вводу від користувача
 phrase = input("Enter a phrase: ").lower()
 letter = input("Enter a letter: ").lower()
 
-# Перевірка на те, що letter є однією буквою
 if len(letter) != 1:
     raise ValueError("The 'letter' parameter should be a single character.")
 
-# Підрахунок входжень та виведення результату
 result = count_occurrences(phrase, letter)
 print(f"The letter '{letter}' occurs {result} times in the phrase.")
