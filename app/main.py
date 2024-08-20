77d70d82d40950afe5b5df0483b8024c832226e8
Kuzmakin <kuzmakin@gmail.com>
2024-08-20 12:01:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     letter_count = 0
-    for i in phrase:
-        if i == letter.lower():
+    for char in phrase:
+        if char == letter.lower():
             letter_count += 1
     return letter_count
 
 
 if __name__ == "__main__":
-    phrase = input("Enter a phrase:").lower()
-    letter = input("Enter a letter:").lower()
+    user_phrase = input("Enter a phrase:").lower()
+    user_letter = input("Enter a letter:").lower()
 
-    result = count_occurrences(phrase, letter)
-    print(f"The letter '{letter}' occurs {result} times in the phrase.")
+    result = count_occurrences(user_phrase, user_letter)
+    print(f"The letter '{user_letter}' occurs {result} times in the phrase.")
+
