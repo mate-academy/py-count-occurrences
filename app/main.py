18ab5e590d740fde9c831efeb3f893b39621635d
Kuzmakin <kuzmakin@gmail.com>
2024-08-20 15:46:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+# app/main.py
+
+# Задаємо змінні різних типів даних
 integer_value = 123
 empty_list = []
 greeting_message = "Hi!"
@@ -7,15 +10,21 @@ boolean_value = True
 key_value_dict = {"key": "value"}
 pi_value = 3.14
 
+# Створюємо словник з двома ключами: "mutable" та "immutable"
 sorted_variables = {
-    "mutable": [empty_list, number_list, key_value_dict],
+    "mutable": [
+        empty_list,  # Список, що можна змінювати
+        number_list, # Ще один список
+        key_value_dict  # Словник
+    ],
     "immutable": [
-        integer_value,
-        greeting_message,
-        number_tuple,
-        boolean_value,
-        pi_value
+        integer_value,  # Ціле число
+        greeting_message,  # Рядок
+        number_tuple,  # Кортеж
+        boolean_value,  # Логічне значення
+        pi_value  # Дійсне число
     ]
 }
 
+# Виводимо результати
 print(sorted_variables)
