9308cba8ccccc9e18c3552c0a876db4bf915789e
Kuzmakin <kuzmakin@gmail.com>
2024-08-20 15:48:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -10,21 +10,21 @@ boolean_value = True
 key_value_dict = {"key": "value"}
 pi_value = 3.14
 
-# Створюємо словник з двома ключами: "mutable" та "immutable"
+
 sorted_variables = {
     "mutable": [
-        empty_list,  # Список, що можна змінювати
-        number_list, # Ще один список
-        key_value_dict  # Словник
+        empty_list,
+        number_list,
+        key_value_dict
     ],
     "immutable": [
-        integer_value,  # Ціле число
-        greeting_message,  # Рядок
-        number_tuple,  # Кортеж
-        boolean_value,  # Логічне значення
-        pi_value  # Дійсне число
+        integer_value,
+        greeting_message,
+        number_tuple,
+        boolean_value,
+        pi_value
     ]
 }
 
-# Виводимо результати
+
 print(sorted_variables)
