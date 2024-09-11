326c625122e01ea41298eacfe14e4fc2036561f2
Evgeniya <didovichevgeniya@gmail.com>
2024-09-11 19:52:26 +0300

    line

--- app/main.py
+++ app/main.py
@@ -15,9 +15,9 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
     """
-
     counter = 0
     for char in phrase:
         if char.lower() == letter.lower():
             counter += 1
     return counter
+
