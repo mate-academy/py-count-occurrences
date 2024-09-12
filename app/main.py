6f2b53db89268554c9dccc6b941090c4ce1199e8
Viktoriia Danylchuk <viktoriadanylchuk@gmail.com>
2024-09-12 12:29:25 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -16,6 +16,5 @@ def count_occurrences(phrase: str, letter: str) -> int:
     """
     counter = 0
     for char in phrase:
-        if char.lower() == letter.lower():
-            counter += 1
+        counter += (char.lower() == letter.lower())
     return counter
