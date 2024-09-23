5c2bd8452c53c189a2d3f1b3f1bf41f44b3a472c
Timofii Lishchuk <pravadaoda0@gmail.com>
2024-09-23 14:18:37 +0300

    VersionAfterReview

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
-from cmath import phase
-
-from pyflakes.checker import counter
-
-
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for char in phrase:
-        if char.lower() == letter.lower():
-             counter += 1
-    return counter
+    """
+    Implement count_occurrences function:
+
+    It takes a phrase and a letter and calculates the number of times
+    the letter appears in the phrase. The function is case insensitive.
+
+    count_occurrences("letter", "t") == 2
+    count_occurrences("abc", "a") == 1
+    count_occurrences("abc", "d") == 0
+    count_occurrences("ABC", "a") == 1
+
+    :param phrase: phrase to count in it
+    :param letter: letter to find occurrences of it
+    :return: count occurrences of letter in phrase
+    """
+
+    return phrase.lower().count(letter.lower())
