53f2ab144f2d08bd1d56aa152ee1d3b0a4c809a9
Kuzmakin <kuzmakin@gmail.com>
2024-09-24 16:37:03 +0300

    Wrote without cycle

--- app/main.py
+++ app/main.py
@@ -1,13 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-
-    letter_count = 0
-    for i in phrase:
-        if i == letter:
-            letter_count += 1
-    return letter_count
-
-
-phrase = input("Enter a phrase:").lower()
-letter = input("Enter a letter:").lower()
-
-count_occurrences(phrase, letter)
+    return phrase.count(letter)
