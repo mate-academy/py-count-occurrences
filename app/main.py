f65591e915e127ad6a0130b1b8acee2c74564965
IrynaBrenko <brenko.iryna1997@gmail.com>
2024-10-08 13:08:08 +0300

    fix change

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    """`
+    """
     Implement count_occurrences function:
 
     It takes a phrase and a letter and calculates the number of times
