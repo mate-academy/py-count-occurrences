91d17b9790018d0171d86e78a7aa88daadb6d8d2
Ihor Moskalenko <ingvarsvensson7@gmail.com>
2024-10-25 23:03:53 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    """
+        Count occurrences of a letter in a phrase (case insensitive).
+
+        :param phrase: The phrase to search within.
+        :param letter: The letter to count occurrences of.
+        :return: The number of occurrences of the letter in the phrase.
+        """
+    counter = 0
+    for char in phrase:
+        if char.lower() == letter.lower():
+            counter += 1
+    return counter
