e4e4ba2dad3b20d4dfcc0baa31a4ae2f10f765e0
Daniil Tsitavets <d.tsitavets@gmail.com>
2024-10-31 21:12:57 +0200

    refactor: rename vars

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
     phrase = phrase.lower()
     letter = letter.lower()
     result = {}
-    for l in phrase:
-        result[l] = result.get(l, 0) + 1
+    for character in phrase:
+        result[character] = result.get(character, 0) + 1
     return result.get(letter, 0)
