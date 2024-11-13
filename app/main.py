a65439513b227510a13a0336d2ff38e579493c25
Mykola Shcherban <91997489+mshcherban1@users.noreply.github.com>
2024-11-13 08:49:43 -0600

    Update main.py
    Syntax error corrected

--- app/main.py
+++ app/main.py
@@ -17,4 +17,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     phrase_lower = phrase.lower()
     letter_lower = letter.lower()
 
-    return phrase_lower.count(letter_lower
+    return phrase_lower.count(letter_lower)
