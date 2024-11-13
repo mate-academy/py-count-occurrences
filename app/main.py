cfcc99c5e060175aa57026d41865c7f331eb50f6
Taras <t.kudinov90@gmail.com>
2024-11-13 13:06:14 +0200

    Recompleted task, accordingly mentors review.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for character in phrase:
-        if character.lower() == letter.lower():
-            counter += 1
+    characters = list(phrase.lower())
+    counter = characters.count(letter.lower())
 
     return counter
