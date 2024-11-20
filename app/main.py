8a2e49181512ccd7a1476acfe2a6c68f6a3a48c4
Volodymir Shlikhta <shlikhta77@gmail.com>
2024-11-20 07:15:33 +0200

    count_occurrences in one line

--- app/main.py
+++ app/main.py
@@ -1,6 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for lit in phrase:
-        if lit.lower() == letter.lower():
-            counter += 1
-    return counter
+    return phrase.lower().count(letter.lower())
