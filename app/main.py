42e002eec880f4bc7dccb6d2efbcc8fef6e7dda0
Danylo <lomporia@gmail.com>
2024-11-23 13:52:40 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    count = 0
-    for char in phrase:
-        if char == letter:
-            count += 1
-    return count
+   count = 0
+   for char in phrase:
+      if char == letter:
+         count += 1
+   return count
