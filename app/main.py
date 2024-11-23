ce12d9d13c0f9c4727beec1eafe437aa96af71a9
Oleksandr Barabanov <imtence@icloud.com>
2024-11-23 12:10:48 +0200

    Formatted-Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-
     count = 0
-
+    pass
     if len(phrase) == 0:
         return 0
 
     for character in phrase:
         if character.lower() == letter.lower():
             count += 1
-
-    return count
+        return count
