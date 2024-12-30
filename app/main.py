4488b1edec92d07bacd794daebe4978ca0e7efac
Vinohradov Volodymyr <eXpertt13@gmail.com>
2024-12-30 18:26:51 +0200

    Implemented the `count_occurrences` function, which calculates the number of occurrences of a specific letter in a given phrase, case-insensitive.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    """
+    count_occurrences("letter", "t") should return 2
+    count_occurrences("abc", "a") should return 1
+    count_occurrences("abc", "d") should return 0
+    count_occurrences("ABC", "a") should return 1
+    :param phrase: text to count in it
+    :param letter: letter to find
+    :return: count occurrences of letter in pharse
+    """
+    return phrase.lower().count(letter.lower())
