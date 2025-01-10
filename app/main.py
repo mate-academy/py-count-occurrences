5841c464a817271100f579d48da9d1f340d55fa1
Mixon <robostorepl@gmail.com>
2025-01-10 19:13:06 +0100

    Something

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for character in phrase:
-        if character.lower() == letter.lower():
-            counter += 1
+    counter = letter.lower()
 
-    return  counter
+    result = phrase.lower().count(counter)
+
+    return  result
