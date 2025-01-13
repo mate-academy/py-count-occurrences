a74fe93d35a374b7982e111e66b9831b704196cd
Dmytro Minyaylo <dimon9660@gmail.com>
2025-01-13 18:35:57 +0200

    fixed py-count-occurrences

--- app/main.py
+++ app/main.py
@@ -1,10 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    phrase = phrase.lower()
-    letter = letter.lower()
-    return phrase.count(letter)
-
-
-print(count_occurrences("letter", "t"))
-print(count_occurrences("abc", "a"))
-print(count_occurrences("abc", "d"))
-print(count_occurrences("ABC", "a"))
+    return phrase.lower().count(letter.lower())
