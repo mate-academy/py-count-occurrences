a6b6eb767ffc1551b2b130a36cf9bb033b287673
Tristan Zinn <tristanzinn@MacBook-Pro-von-Tristan.local>
2025-01-20 15:43:18 +0100

    nothing changed

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 
 def count_occurrences(phrase: str, letter: str) -> int:
+    
+    # write your code here
     d = defaultdict(int)
     for c in phrase:
         d[c] += 1
 
     return d[letter]
-    # write your code here
-    
