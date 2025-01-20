d4958359367e0baac491f85f12866e554ea1d8e5
Tristan Zinn <tristanzinn@MacBook-Pro-von-Tristan.local>
2025-01-20 15:01:51 +0100

    Lösung wurde hinzugefügt

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    d = defaultdict(int)
+    for c in phrase:
+        d[c] += 1
+
+    return d[letter]
     # write your code here
-    pass
+    
