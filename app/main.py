acdbff91a041c227e307f88e7182b874ec5ee0f4
Yevhenii <110053436+Yevhenii0536@users.noreply.github.com>
2025-01-23 20:28:50 +0200

    fix: code review issues

--- app/main.py
+++ app/main.py
@@ -1,5 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    phrase_lower = phrase.lower()
-    letter_lower = letter.lower()
-
-    return phrase_lower.count(letter_lower)
+    return phrase.lower().count(letter.lower())
