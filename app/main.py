b5a55d2485711991bbae297fb259a3e3ca4cba69
Ioann Nadtocheev <ioannnatocheev@gmail.com>
2025-02-05 20:04:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
-def count_occurrences(phrase: str, letter: str) -> int:
+my_integer = 123
+my_string = "Hi!"
+my_list = [1, 2]
+my_float = 222.48
+my_boolean = True
+my_dict = {"name": "Maria"}
+my_set = {1, 2, 3}
+fruit_tuple = ("apple", "banana")
+my_boolean2 = False
 
-    counter = 0
-    for char in phrase:
-        if char.lower() == letter.lower():
-            counter += 1
-    return counter
+sorted_variables = {
+    "mutable": [my_list, my_dict, my_set],
+    "immutable": [my_integer, my_string, my_float, my_boolean,
+                  my_boolean2, fruit_tuple
+                  ]
+}
