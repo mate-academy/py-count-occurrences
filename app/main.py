e225938d8f096cb6345b1d5eb70f3c22116afc70
Serhii-Leonenko <leonenko3149@gmail.com>
2025-02-10 10:44:44 +0200

    added docstring

--- app/main.py
+++ app/main.py
@@ -1,2 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+    Function which counts number of letters
+    :param phrase:
+    :param letter:
+    :return:
+    """
     return phrase.lower().count(letter.lower())
