671431ab6dabbf9d823ac3f039e73875e5cef69a
hemloeurt <h.loeurt@gmail.com>
2025-02-16 13:41:08 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
     pass
     print(f"Checking occurrences of '{letter}' in '{phrase}'")
     result = phrase.lower().strip().count(letter.lower().strip())
-    #trim space then lower case -> and count letter
+    # trim space then lower case -> and count letter
     print(f"Count result: {result}")
-    return  result
+    return result
