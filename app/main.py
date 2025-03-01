cb93d31b306ac58b19af300cb575c2a083ed782c
Danya <velichayshiy577@gmail.com>
2025-03-01 23:47:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
+    res = 0
+    for b in phrase:
+        if b == letter:
+            res += 1
+    print(res)
+
+    return int(res)
     pass
+
+count_occurrences('sdaasdasddsa', 's')
