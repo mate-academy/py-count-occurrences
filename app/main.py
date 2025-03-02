9994280edad2afee5d7868b6392e6ec1017a00c7
Danya <velichayshiy577@gmail.com>
2025-03-02 20:48:38 +0200

    exercise done

--- app/main.py
+++ app/main.py
@@ -1,11 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    res = 0
-    for b in phrase:
-        if b == letter:
-            res += 1
-    print(res)
-
-    return int(res)
-    pass
-
-count_occurrences('sdaasdasddsa', 's')
+    return phrase.lower().count(letter.lower())
