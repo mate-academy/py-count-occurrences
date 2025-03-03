a12ba91adbb1eba93ce3ca26be3cfefcdd43f16f
Paddy O'Connor <patrick_oconnor@hotmail.co.uk>
2025-03-03 17:19:43 +1100

    Solution

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
             new_dict[i] += 1
 
     for k, v in new_dict.items():
-        if k == letter:
+        if k == letter.lower():
             return v
         
-print(count_occurrences("letTer", "t"))
+print(count_occurrences("letter", "T"))
