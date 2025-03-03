c53369b7b2ba19a16b1f313e123ae6ee37d253c3
Paddy O'Connor <patrick_oconnor@hotmail.co.uk>
2025-03-03 17:24:55 +1100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
-    new_dict = {}
-    for i in phrase.lower():
-        if i not in new_dict:
-            new_dict[i] = 1
-        else:
-            new_dict[i] += 1
 
-    for k, v in new_dict.items():
-        if k == letter.lower():
-            return v
+    count = phrase.lower().count(letter.lower())
+
+    return count
+    # new_dict = {}
+    # for i in phrase.lower():
+    #     if i not in new_dict:
+    #         new_dict[i] = 1
+    #     else:
+    #         new_dict[i] += 1
+
+    # for k, v in new_dict.items():
+    #     if k == letter.lower():
+    #         return v
         
-print(count_occurrences("letter", "T"))
+count_occurrences("leTter", "T")
