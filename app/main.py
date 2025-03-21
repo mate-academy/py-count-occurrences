4a3a42b9763192e28e1ba65d274ce44ac82dbd4e
Linka <s.syvokorovska@gmail.com>
2025-03-21 23:29:15 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    """
+    Count occurrences of a letter in a phrase (case insensitive).
+
+    :param phrase: The phrase to search within.
+    :param letter: The letter to count occurrences of.
+    :return: The number of occurrences of the letter in the phrase.
+    """
+
+    counter = 0
+
+    for char in phrase:
+        if char.lower() == letter.lower():
+            counter += 1
+
+    return counter
+
+
+print(count_occurrences("Hello, world!", "l"))
+print(count_occurrences("HeLlo, world!", "l"))
