8369a1354170dd3464e2726dd613e70803d1dded
Ostap Holyboroda <ostapgoliboroda@gmail.com>
2025-03-23 14:28:26 +0100

    count_occurrences

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    count = 0
+    for symbol in phrase:
+        if symbol.lower() == letter.lower():
+            count += 1
+    return count
