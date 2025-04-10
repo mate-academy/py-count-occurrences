e1120372d1e1a545d1c49256aa00d2563c6b6cc9
mkonyakhin <konyakhin.m@gmail.com>
2025-04-10 22:29:06 +0100

    make git pull --rebase
    # Conflicts:
    #	app/main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for character in phrase.lower():
-        if character == letter.lower():
-            counter += 1
-
-    return counter
+    return phrase.lower().count(letter.lower())
