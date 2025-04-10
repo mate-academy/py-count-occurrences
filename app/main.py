ee0527a1391de64e40e7dfd0d4aa65fbf6abaf71
mkonyakhin <konyakhin.m@gmail.com>
2025-04-10 22:06:56 +0100

    fix var name

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     counter = 0
-    for element in phrase.lower():
-        if element == letter.lower():
+    for character in phrase.lower():
+        if character == letter.lower():
             counter += 1
 
     return counter
