fb7c13067682284fe5595f54651fe41078983973
Kostya Mileant <kostya.milya@gmail.com>
2025-04-11 19:23:35 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -17,7 +17,8 @@ def count_occurrences(phrase: str, letter: str) -> int:
 
     count = 0
     for i in phrase:
-        if i.lower() == letter.lower():
+        if (i.lower() ==
+letter.lower()):
             count += 1
 
     return count
