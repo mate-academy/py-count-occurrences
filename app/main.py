1658843917e898d1d40e14744c5dac518678e7e1
vladkuc <voramok573@gmail.com>
2025-04-11 22:28:16 -0400

    Update main.py
    # edited

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
     pass
- def count_occurrences(words):
-    return len(words)  # тимчасовий код, щоб GitHub побачив зміни
+# edited by Vlad for Mate Academy task ✅
 
