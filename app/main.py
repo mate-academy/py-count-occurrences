593f91d3b5895bc38fc115adaa96312ed1f5a094
vladkuc <voramok573@gmail.com>
2025-04-11 22:26:01 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
     pass
+ def count_occurrences(words):
+    return len(words)  # тимчасовий код, щоб GitHub побачив зміни
+
