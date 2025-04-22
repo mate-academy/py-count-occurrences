bb7937c8a91398ed0dd1eb9737a885ce5b0e4ef1
Vitrikus Yuriy <vitrikus007@gmail.com>
2025-04-22 22:44:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    counter = 0  # ініціалізація лічильника
+
+    for char in phrase:  # перебір символів у рядку
+        if char.lower() == letter.lower():  # перевірка регістру
+            counter += 1  # збільшуємо лічильник
+
+    return counter  # повертаємо результат після циклу
+
