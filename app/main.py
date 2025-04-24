6207b24c8c648ae40b7141285d5eb6eccd52c5f3
Samuel Yudi <toyotaa038@gmail.com>
2025-04-24 18:45:41 -0300

    Fix char name + add new line

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     counter = 0
     letter = letter.lower()
-    for char in phrase.lower():
-        if char == letter:
+    for character in phrase.lower():
+        if character == letter:
             counter += 1
+
     return counter
