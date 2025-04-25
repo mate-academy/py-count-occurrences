5c39139b2cf7e3180fdd9761cb62385be7538fa4
Vitrikus Yuriy <vitrikus007@gmail.com>
2025-04-25 22:33:39 +0300

    Виправив змінні у `sorted_variables`

--- app/main.py
+++ app/main.py
@@ -1,2 +1,14 @@
 sorted_variables = []
 
+# Додаємо змінні до списку
+sorted_variables.extend([
+    "lucky_number",
+    "pi",
+    "one_is_a_prime_number",
+    "name",
+    "my_favourite_films",
+    "profile_info",
+    "marks",
+    "collection_of_coins",
+])
+
