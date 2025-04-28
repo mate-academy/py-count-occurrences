5fe3f1cd8d2d33276465f4a7ee636c4ccbaeec6b
Ihor Pershin <issue.07.08.09@gmail.com>
2025-04-28 18:46:40 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    count_of_letter = 0  # рахуємо кількість заданних літер у слові
+    for i in list(phrase.lower()):  # для кожного елемента слова
+        if i == letter.lower():  # задаємо те, з чим порівняти кожен елемент
+            count_of_letter += 1  # додаємо +1 до підрахунку кількості літер
+    return count_of_letter  # повертаємо результат підрахунків
