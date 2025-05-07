981c6509e920f5d1844970fb0a9c7cfe40ddd2f1
Yaroslav Korotenko <Kor1k13002@gmail.com>
2025-05-07 22:04:13 +0300

    pull request 1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    count = 0
+    for i in phrase:
+        if i.lower() == letter.lower():
+            count += 1
+    print (count)
+
+count_occurrences("-aaafgkytyukabcaa", "a")
+
