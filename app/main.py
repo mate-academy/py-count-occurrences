930b95d3a1e1e7823fdc8b211b9e926c2b00f119
Nicolas-Jacob <nicolasoliveirajacob@gmail.com>
2025-05-09 12:23:36 -0300

    fix char name

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     counter = 0
-    for char in phrase:
-        if char == letter.lower():
+    for character in phrase:
+        if character == letter.lower():
             counter += 1
         return counter
