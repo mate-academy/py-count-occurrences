71e2a8dc2eaa7978b09dbcb5ee4a84c9e741a8ce
Dmytro Bondar <catellio2016@gmail.com>
2025-05-21 22:20:29 +0300

    Fixed issues

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-
     phrase = phrase.lower()
-    letter = letter.lower()
-    
+    letter = letter.lower()   
     return phrase.count(letter)
