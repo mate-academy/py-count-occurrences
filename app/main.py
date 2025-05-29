62a916bf28555df42d5e1fdeecebdc115d7e15fc
foxytail8 <xinenex@gmail.com>
2025-05-29 10:25:10 +0300

    Solution after AI check

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
     letter_lower = letter.lower()
     count = 0
     for letter_unit in phrase_lower:
-        if phrase_lower and letter_unit == letter_lower:
+        if letter_unit == letter_lower:
             count += 1
     return count
