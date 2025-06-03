0f176ed15ea15393225de801387a778c5ff32f3a
Felipe Oliveira <72achiever72@gmail.com>
2025-06-03 16:44:24 -0300

    Fixed letter.lower() in loop

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     counter = 0
+    letter = letter.lower()
     for char in phrase.lower():
-        if char == letter.lower():
+        if char == letter:
             counter += 1
     return counter
