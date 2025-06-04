18ad28747eed8bcaf561b73f94a785dfae31925b
Daniil Ponomarov <smokequitz@gmail.com>
2025-06-04 16:33:41 +0100

    changed programme logic

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
 
     count = 0
     for letters in phrase:
-        if letter.lower() in letters.lower():
+        if letter.lower() == letters.lower():
             count += 1
     return count
