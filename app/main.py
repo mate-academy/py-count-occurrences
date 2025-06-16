b76d2ef6a0d20009b408f2edc590a92a8c459f93
Oleksandr Pivtorak <Sanjok150397@gmail.com>
2025-06-16 21:08:29 +0300

    Changed to oneliner

--- app/main.py
+++ app/main.py
@@ -1,6 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for char in phrase:
-        if char.lower() == letter.lower():
-            counter += 1
-    return counter
+    return phrase.lower().count(letter.lower())
