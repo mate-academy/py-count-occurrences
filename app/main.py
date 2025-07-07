8324330efe28be8b469f78338eccd63d6e877f03
vasylhladyshev404 <vasyl@nextgear.nl>
2025-07-07 14:12:46 +0200

    2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     result = 0
-    phrase = ''.split(phrase)
+    phrase = "".split(phrase)
     for i in range(0, len(phrase)):
         if phrase[i] == letter:
             result += 1
