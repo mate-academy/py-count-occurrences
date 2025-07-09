7c924e6bb83af213328870e48f0ee2dc6752a5d3
Álefe Lins <alefelins01@gmail.com>
2025-07-09 14:26:05 -0300

    feat: função para contar ocorrências de uma letra em uma frase e testes

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
+    count = 0
+    for char in phrase.lower():
+        if char == letter.lower():
+            count += 1
+    return count
+
+
     pass
