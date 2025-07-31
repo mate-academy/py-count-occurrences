047e904b70f962e4885d33c4ed32f408d1491dc7
Aleksandr Vilenskiy <mailsuperalex@gmail.com>
2025-07-31 20:04:57 +0300

    SDELAL VSE

--- app/main.py
+++ app/main.py
@@ -2,5 +2,5 @@ def count_occurrences(phrase: str, letter: str) -> int:
     count = 0
     for i in phrase:
         if i.upper() == letter.upper():
-            count+=1
+            count += 1
     return count
