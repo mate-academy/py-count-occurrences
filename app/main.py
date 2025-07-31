444ff6838d57da7c5714c06eb9fb9c4a5bebdc0a
Nelson <nelsonespiritu21@gmail.com>
2025-07-31 13:35:35 +0800

    Solution

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     for i in phrase:
         if i == letter:
             cou += 1
+
     return cou
 
 occurrence = count_occurrences("letter", "t")
