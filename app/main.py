3ab8e05c27578fffb06a5caa18a52cdeee1ea8c6
Radoslaw Ptak <radek1026@interi.pl>
2025-08-11 21:05:50 +0200

    Change in Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
 
-    counter = 0
-    for character in phrase:
-        if character.lower() == letter.lower():
-            counter += 1
-
-    return counter
+    return phrase.lower().count(letter.lower())
