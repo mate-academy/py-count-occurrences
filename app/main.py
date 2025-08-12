155aef3745ec779641504dc64438c6741810b15a
Vitalii <vitalyoirshko@gmail.com>
2025-08-12 16:18:16 +0300

    fix line according flake

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     """
-       Counts the number of times 'letter' appears in 'phrase', case-insensitively.
-
+       Counts the number of times 'letter' appears in 'phrase'.
        :param phrase: The phrase to search within.
        :param letter: The letter to count occurrences of.
        :return: The number of occurrences of the letter in the phrase.
