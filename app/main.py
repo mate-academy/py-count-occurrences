488466747a2832b220f3788d4d3fcaef3d6daab0
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-08-23 14:04:51 +0200

    solution #2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     counter = 0
     for l in phrase.lower():
-        if l == letter:
+        if l == letterlower():
             counter += 1
     return counter
