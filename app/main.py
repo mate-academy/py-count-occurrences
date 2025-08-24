1c447c6794345a6fb72a7982df9ce343aca419d3
Nazar <your@mail.com>
2025-08-24 13:43:41 +0300

    flake8_upd

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     """
-    Count occurrences of letter in phrase case-insensitively and return the integer count.
+    Count occurrences of letter in phrase case-insensitively
+    and return the integer count.
 
     Parameters:
     - phrase: str is (the input string to search)
     - letter: str is (the character or substring to count; case-insensitive)
 
     Returns:
-    - int: Count occurrences of letter in phrase case-insensitively and return the integer count."
+    - int: Count occurrences of letter in phrase case-insensitively
+    and return the integer count.
 
     """
     return phrase.lower().count(letter.lower())
