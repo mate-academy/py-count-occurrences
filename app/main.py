c65fc0cf669caf5f7fc162f677069ac9985102c6
Kateryna Ivanenko <kateryna578ivanenko@gmail.com>
2025-08-24 18:28:55 +0300

    Solution4

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
-"""
-Parameters:
-- phrase (str): The input text to search.
-- letter (str): A single-character string to count.
-Returns:
-- int: The number of occurrences of `letter` in `phrase`.
-"""
-
-
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+    Parameters:
+    - phrase (str): The input text to search.
+    - letter (str): A single-character string to count.
+    Returns:
+    - int: The number of occurrences of `letter` in `phrase`.
+    """
     return phrase.lower().count(letter.lower())
