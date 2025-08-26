7d60fd373ed5356d7067422e363cf65b40642358
Victor Borden <vborden2022@fau.edu>
2025-08-25 21:38:28 -0400

    removed-loop

--- app/main.py
+++ app/main.py
@@ -4,9 +4,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
     Returns: int count
     """
     lower_letter = letter.lower()
-    phrase_list = list(phrase)
-    count = 0
-    for i in range(len(phrase_list)):
-        if phrase_list[i].lower() == lower_letter:
-            count += 1
+    phrase_list = phrase.lower()
+    count = phrase_list.count(lower_letter)
     return count
