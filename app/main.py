9c594d8da01b0220b11d4a9135152680f66448c0
Oskar Szeliga <oscarek555@gmail.com>
2025-08-28 18:05:38 +0200

    Updated Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+# phrase: A string in which to count occurrences of a letter.
+# letter: The letter whose occurrences need to be counted in the given phrase.
+# return phrase.count(letter): returns the number of times the letter appears in the phrase, while being case insensitive.
 def count_occurrences(phrase: str, letter: str) -> int:
     phrase = phrase.lower()
     letter = letter.lower()
