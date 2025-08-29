a74102b12a00150ee00b679c7b1ffbd1da12cc79
avozniuk <a.o.vozniuk@gmail.com>
2025-08-29 13:57:40 +0300

    updates

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     result = 0
     for character in phrase:
-        if character.lower() == letter.lower() or character.upper() == letter.upper():
+        if (character.lower() == letter.lower()
+                or character.upper() == letter.upper()):
             result += 1
     return result
