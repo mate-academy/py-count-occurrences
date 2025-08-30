d21511cb100e22932122537e15ce2cc45d75f5a0
acidcor <deadfoxua1337@gmail.com>
2025-08-30 13:03:20 +0200

    bad code fix

--- app/main.py
+++ app/main.py
@@ -1,2 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    return phrase.count(letter) + phrase.count(letter.upper())
+    return phrase.count(letter.lower()) + phrase.count(letter.upper())
