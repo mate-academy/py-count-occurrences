7701e3da265f4764cafc12635e1c9248fc2a7a4d
Konrad <kodorek123@gmail.com>
2025-08-31 22:19:33 +0200

    implement py-count-char

--- app/main.py
+++ app/main.py
@@ -2,11 +2,9 @@ from cmath import phase
 
 
 def count_occurrences(phrase: str, letter: str) -> int:
-    if len(letter) != 1:
-        return 0
 
     count = 0
-    for letter in phrase:
-        if letter.lower() == letter.lower():
+    for char in phrase:
+        if char.lower() == letter.lower():
             count += 1
     return count
