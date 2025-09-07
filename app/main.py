3b0471d8b6d7a3c7e72008223965ff929b98b5bf
Andrii Vasylenko <gods16garage@gmail.com>
2025-09-07 20:57:48 +0300

    fix newline and character

--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
-def count_occurrences(phrase: str, letter: str) -> int:
+def count_occurrences(phrase: str, substring: str) -> int:
     """
-    Count occurrences of a letter in a phrase (case insensitive).
+    Count occurrences of a substring in a phrase (case insensitive).
     :param phrase: The phrase to search within.
-    :param letter: The letter to count occurrences of.
-    :return: The number of occurrences of the letter in the phrase.
+    :param substring: The substring to count occurrences of.
+    :return: The number of occurrences of the substring in the phrase.
     """
-    counter = 0
-    for char in phrase:
-        if char.lower() == letter.lower():
-            counter += 1
-    return phrase.lower().count(letter.lower())
+    return phrase.lower().count(substring.lower())
