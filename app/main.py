4aeb90030fe233d41bfbeab3ae8fdae577b0d695
Olena Pyslar <pislar33@gmail.com>
2025-09-09 22:17:28 +0300

    fix comment one more time 3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    if letter == "":
-        return 0
-    else:
-        return phrase.lower().count(letter.lower())
+    return phrase.lower().count(letter.lower())
