de68edc6000c2ed1081cd684d87592138c5b95ea
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-09-10 12:00:43 -0300

    code description correct

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    #Retorna o número de vezes que a determinada letra aparece na frase.
-    #O uso do lower() é para garantir a contagem(count) independente de maiúsculas ou minúsculas.
+    # Retorna o número de vezes que a determinada letra aparece na frase.
+    # O uso do lower() é para garantir a contagem(count) independente
+    # de maiúsculas ou minúsculas.
     return phrase.lower().count(letter.lower())
