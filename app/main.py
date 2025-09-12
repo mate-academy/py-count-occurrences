e644c3fce16f5a59d1d946677012e550920d1ffb
henriquejhansen <henriquejhansen@gmail.com>
2025-09-11 23:06:50 -0300

    Update main.py
    teste3

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-
+def count_occurrences(phrase: str, letter: str) -> int:
+    counter = 0
+    for char in phrase.lower():
+        if char == letter.lower():
+            counter += 1
+    return counter
