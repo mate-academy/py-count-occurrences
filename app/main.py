bae7851ef4f9cc2c83db0c1b2a83d72269217ab4
ten4i <oleksijchunihin@gmail.com>
2025-09-13 19:55:45 +0300

    fixed small issues with flake8

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    #write your code here
-    if len(letter) == 1: 
+    # write your code here
+    if len(letter) == 1:
         return phrase.lower().count(letter.lower())
     else:
         return 0
