a1261c488445582281b43cef9bf5a13140049dc2
Timur Chermoshentsev <ctv2140@gmail.com>
2025-09-14 19:07:17 +0200

    Add docstring and optimize count_occurrences

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    count = 0
-    for i in phrase:
-        if i.lower() == letter.lower():
-            count += 1
-    return count
+    """
+        Count the number of occurrences of a character in a string (case-insensitive).
+
+        Args:
+            phrase (str): The string in which to search.
+            letter (str): The character to count.
+
+        Returns:
+            int: The number of times the character appears in the string.
+        """
+    if not letter:
+        return 0
+
+    return phrase.lower().count(letter.lower())
