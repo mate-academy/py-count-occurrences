a239f22e32e699c74fad52289ae9f6da3266680b
Bohdan Smyvalov <bogdansmyvalov@gmail.com>
2025-09-15 12:47:13 +0300

    fix error W293 blank line contains whitespace

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     counter = 0
-    
     for char in phrase.lower():
         counter += 1 if char.lower() == letter.lower() else 0
-        
     return counter
