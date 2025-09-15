f9538cad69643cca0a226479a9e380104f22259a
Bohdan Smyvalov <bogdansmyvalov@gmail.com>
2025-09-15 13:37:55 +0300

    add recommendations

--- app/main.py
+++ app/main.py
@@ -1,2 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    if len(phrase) == 0 or len(letter) != 1:
+        return 0
+    if not letter:
+        return 0
     return phrase.lower().count(letter.lower())
