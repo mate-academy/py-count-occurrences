f89030cebfe44a7c2df783523b9b9b4a19ec077e
Bohdan Smyvalov <bogdansmyvalov@gmail.com>
2025-09-15 12:56:42 +0300

    fix error Missing docstring (required)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for char in phrase.lower():
-        counter += 1 if char.lower() == letter.lower() else 0
-    return counter
+    return phrase.lower().count(letter.lower())
