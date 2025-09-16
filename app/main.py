9b88b85a32fba080077d8f128dc6ec9ab0194648
Nikita Grinyov <nikita3grynov@gmail.com>
2025-09-16 17:34:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
-def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+def count_occurences(phrase: str, letter: str) -> int:
+    phrase = phrase.lower()
+    letter = letter.lower()
+    _dict = {letter: 0}
+    for let in phrase:
+        if let in _dict:
+            _dict[letter] += 1
+    return _dict[letter]
