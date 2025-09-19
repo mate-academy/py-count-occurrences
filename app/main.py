7012f5e023987e87e6cdd2af02590cc39533cd01
noname <noname@gmail.com>
2025-09-19 16:08:41 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
     counter = 0
 
     for char in phrase:
-        if phrase.lower() == letter.lower():
+        if char.lower() == letter.lower():
             counter += 1
     return counter
