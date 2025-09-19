428580016e622ed61f5ade1e578352f9900086f2
noname <noname@gmail.com>
2025-09-19 17:27:41 +0200

    new fix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+
+    if len(letter) != 1:
+        raise ValueError("Параметр 'letter' має бути одиничним символом.")
+
     counter = 0
 
     for char in phrase:
         if char.lower() == letter.lower():
             counter += 1
+            
     return counter
