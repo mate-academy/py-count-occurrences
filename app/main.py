4888037bc224e7f7681a94bb3485aa5e72cc5ba0
steven42069 <stevendiaz152@gmail.com>
2025-09-22 22:50:41 -0400

    something about the discription

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
-def count_occurrences(phrase: str, letter: str) -> int:
+"""
+Count occurrences of a letter in a phrase (case insensitive).
+
+:param phrase: The phrase to search within.
+:param letter: The letter to count occurrences of.
+:return: The number of occurrences of the letter in the phrase.
+"""
+
+def count_occurrences(phrase: str, letter: str) -> int:   
     index = phrase.lower().count(letter.lower())
     return index
