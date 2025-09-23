54288e505125889507451dd9097e969a3b47ac6d
steven42069 <stevendiaz152@gmail.com>
2025-09-22 23:21:14 -0400

    i might need some help

--- app/main.py
+++ app/main.py
@@ -9,3 +9,4 @@ Count occurrences of a letter in a phrase (case insensitive).
 def count_occurrences(phrase: str, letter: str) -> int:   
     index = phrase.lower().count(letter.lower())
     return index
+
