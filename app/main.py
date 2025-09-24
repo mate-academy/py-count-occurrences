ae5d7d7b3f9a8bce5edc9f902cb044b404c09ffd
Oleh Bozhak <olbozhak@gmail.com>
2025-09-24 17:59:09 +0300

    fix c

--- app/main.py
+++ app/main.py
@@ -1,2 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    return sum(map(lambda w: 1 if w.lower() == letter.lower() else 0, phrase))
+    return sum(map(lambda c: 1 if c.lower() == letter.lower() else 0, phrase))
