7eff731a801e2acf7ec8adc70104ffc6cc8fa14a
Nikita <hkorobko@gmail.com>
2025-09-26 01:18:39 +0300

    Виправив структуру: прибрав вкладений репозиторій

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     counter = 0
-    for character in phrase:
-        if character.lower() == letter.lower():
+    for char in phrase:
+        if char.lower() == letter.lower():
             counter += 1
-
     return counter
