7802fbbe8da77b4380b0c1838ff8603543afcbc8
mac <mac@MacBook-Air-mac.local>
2025-09-28 18:59:01 +0300

    fix newline

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+    Count occurrences of a letter in a phrase (case insensitive).
+
+    :param phrase: The phrase to search within.
+    :param letter: The letter to count occurrences of.
+    :return: The number of occurrences of the letter in the phrase.
+    """
     counter = 0
     for quantity in phrase:
         if quantity.lower() == letter.lower():
