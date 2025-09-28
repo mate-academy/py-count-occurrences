f6b982575328409a5151ef767582fc0a2d543d84
mac <mac@MacBook-Air-mac.local>
2025-09-28 19:10:03 +0300

    new change

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
 
     :param phrase: The phrase to search within.
     :param letter: The letter to count occurrences of.
-    :return: The number of occurrences of the letter in the phrase.
+    :return: The number of occurrences of the letter .
     """
     counter = 0
     for quantity in phrase:
