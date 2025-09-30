ee5bbe3b52218a714c5047060212bc3ba232b10d
Rodrigo Pertile <rodrigo.pertile1@gmail.com>
2025-09-30 08:28:51 -0300

    Código corrigido

--- app/main.py
+++ app/main.py
@@ -1,2 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    return phrase.lower().count(letter.lower())
+    count = 0
+    for char in phrase:
+        if char.lower()== letter.lower():
+            count += 1
+    return count
