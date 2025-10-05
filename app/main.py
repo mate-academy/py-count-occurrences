554392a55292be65735f9c3316a1832681b2c5d6
Hmjr03 <hmanhani@hotmail.com>
2025-10-06 01:36:16 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,11 +5,9 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: The letter whose occurrences need to be counted.
     :return: The number of times the letter appears in the phrase.
     """
-    # 1. Validacao (para lidar com a sugestao de erro de `letter` de tamanho 1)
-    
+    # Validacao para garantir que `letter` parameter must be a single character.
     if len(letter) != 1:
         raise ValueError("The `letter` parameter must be a single character.")
-    # 2. Solucao `Case Insensitive` e Contagem (usando funcoes built-in) 
     #    Tanto a frase quanto a letra sao convertidas para minusculas antes da contagem.
     return
 phrase.lower().count(letter.lower())
