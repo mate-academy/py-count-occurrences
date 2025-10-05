ceeaa2554d6219fada5d528fb8b6eeb7f238572f
Hmjr03 <hmanhani@hotmail.com>
2025-10-06 01:04:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    """
-    Count how many times a letter appears in a phrase.
-    """
     if len(letter) != 1:
         return phrase.lower().count(letter.lower())
