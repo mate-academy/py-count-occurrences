7211576e5236cd078963827dccb76714664075f0
Hmjr03 <hmanhani@hotmail.com>
2025-10-06 01:57:46 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     if len(letter) != 1:
         raise ValueError("The letter parameter must be a single character."
                         )
-        return phrase.lower().count(letter.lower())
+    return phrase.lower().count(letter.lower())
