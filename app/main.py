ad4d9ad7ec04f8bc6ce0c8bbbf8d25282df968d4
Hmjr03 <hmanhani@hotmail.com>
2025-10-07 03:17:41 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,13 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    if not letter:  # empty string or None
-        return 0
+    """
+    Conta quantas vezes uma letra aparece em uma frase (sem diferenciar maiúsculas e minúsculas).
+
+    :param phrase: A frase onde será feita a contagem.
+    :param letter: A letra a ser contada.
+    :return: O número de ocorrências da letra na frase.
+    """
+    # validação: letter deve ter exatamente 1 caractere
+    if len(letter) != 1:
+        raise ValueError("O parâmetro 'letter' deve conter exatamente um caractere.")
+    
     return phrase.lower().count(letter.lower())
