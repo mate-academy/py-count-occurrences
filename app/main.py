13b72c23a699d573d74399458d5e2ea20bd00f30
victoroli0576-ship-it <victor.oli0576@gmail.com>
2025-10-07 10:10:08 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     count = 0
-    letter = letter.lower
+    letter = letter.lower()
     for char in phrase.lower():
         if char == letter:
             count += 1
     return count
+    
