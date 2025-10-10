8ed20baf834973851f8f4b0ef80090f9c39e2997
Aquilla Rapax <cyrill.muchopad@gmail.com>
2025-10-10 14:35:21 +0100

    Aded variable description

--- app/main.py
+++ app/main.py
@@ -1,2 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+Count occurrences of a letter in a phrase (case insensitive).
+
+:param phrase: The phrase to search within.
+:param letter: The letter to count occurrences of.
+:return: The number of occurrences of the letter in the phrase.
+"""
     return (phrase.lower().count(letter.lower()))
