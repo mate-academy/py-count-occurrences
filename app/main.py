cfe519be3b74e5c24bec626ad402b96b7277b0ec
Ertyrten <101129428+Ertyrten@users.noreply.github.com>
2025-10-10 14:55:59 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     """
     Count occurrences of a substring (letter/word) in a phrase (case insensitive).
-    
+
     :param phrase: The phrase to search within.
     :param letter: The substring to count occurrences of.
     :return: The number of occurrences of the substring in the phrase.
     """
-    
-    # Конвертуємо обидва рядки в нижній регістр для регістронезалежного підрахунку
+
+    # Конвертуємо обидва рядки в нижній регістр
     lowercase_phrase = phrase.lower()
     lowercase_letter = letter.lower()
-    
-    # Використовуємо вбудований метод count(), який підтримує підрахунок рядків будь-якої довжини
+
+    # Використовуємо вбудований метод count()
     return lowercase_phrase.count(lowercase_letter)
