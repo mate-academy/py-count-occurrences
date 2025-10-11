f62a24c87ce80c36acf1f243f92899047e065c93
Denys Zarovnyi <denis20040506@gmail.com>
2025-10-11 22:16:19 +0200

    changed character and added newline

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     if len(phrase) != 0:
         counter = 0
-        for i in phrase:
+        for character in phrase:
             if i.lower() == letter.lower():
                 counter += 1
     else:
         return 0
+
     return counter
