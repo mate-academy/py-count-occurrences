3b87a6e794ce71b39b27fc652f124420ae22bda6
Denys Zarovnyi <denis20040506@gmail.com>
2025-10-12 11:15:30 +0200

    Fixed character in for loop

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     if len(phrase) != 0:
         counter = 0
         for character in phrase:
-            if i.lower() == letter.lower():
+            if character.lower() == letter.lower():
                 counter += 1
     else:
         return 0
