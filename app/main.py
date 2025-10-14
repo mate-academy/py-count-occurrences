d9e5ff2b4bfbfc1e848eb845ef46a19d17d87bf9
Станіслав Кішечко <skishechko@iproject.com.ua>
2025-10-14 14:07:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
-
-
 def count_occurrences(phrase: str, letter: str) -> int:
     """
-    Counts the occurrences of a specific letter in a given phrase, ignoring case.
+    Counts the occurrences of a specific letter in a given
+    phrase, ignoring case.
 
-    This function takes a phrase and a letter, both as string inputs, and determines
-    how many times the specified letter appears in the phrase, regardless of case.
+    This function takes a phrase and a letter, both as string
+    inputs, and determines
+    how many times the specified letter appears in the phrase,
+    regardless of case.
 
-    :param phrase: The string in which to search for the occurrences of the letter.
-    :param letter: The letter whose occurrences need to be counted in the phrase.
-    :return: The number of times the specified letter appears in the given phrase.
+    :param phrase: The string in which to search for the occurrences
+     of the letter.
+    :param letter: The letter whose occurrences need to be counted
+    in the phrase.
+    :return: The number of times the specified letter appears in the
+    given phrase.
     :rtype: int
     """
     return phrase.lower().count(letter.lower())
