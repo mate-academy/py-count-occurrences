250bba765bfc08dcc7481a5750c20a2581de4ff4
Bohdan Kutsyi <bodikoen@gmail.com>
2025-10-15 15:30:29 +0300

    replaced method lover() -> casefold()

--- app/main.py
+++ app/main.py
@@ -10,4 +10,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     if not phrase or not letter:
         return 0
 
-    return phrase.lower().count(letter.lower())
+    return phrase.casefold().count(letter.casefold())
