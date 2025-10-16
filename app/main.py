d1542438b79b76bb8026e7001f7a40d0248d3e48
Oleksii Shcherbak <qksardas001@mail.com>
2025-10-16 15:14:58 +0300

    implement py-count-occurrences

--- app/main.py
+++ app/main.py
@@ -2,4 +2,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     """
     Рахує кількість входжень символу в рядку без урахування регістру.
     """
+    if not letter:
+        raise ValueError("letter cannot be empty")
+
     return phrase.lower().count(letter.lower())
