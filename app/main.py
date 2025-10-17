c3280b13bdb16a4a45f8ce78c765b396ff393cfc
Vladyslav Kramarenko <kramarenko.vlad95@gmail.com>
2025-10-17 11:52:10 +0300

    Complete

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     """
         Count occurrences of a letter in a phrase (case insensitive).
-        Args:
             phrase (str): The text in which to search for the letter.
             letter (str): The letter to count. Should be a non-empty string.
-        Returns:
+        Return:
             int: The number of times the letter appears in the phrase.
         """
     if letter == "":
         raise ValueError("letter must be non-empty")
-
     return phrase.lower().count(letter.lower())
