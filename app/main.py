d0869c7f417bd7b02f37b2d8901df6494744ef85
Farlinder <sashakost2006e@gmail.com>
2025-10-17 18:00:19 +0200

    'add description and make shorter'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for symb in phrase:
-        if symb.lower() == letter.lower():
-            counter += 1
-    return counter
+    """
+    Count occurrences of a letter in a phrase (case insensitive).
+    :param phrase: The phrase to search within.
+    :param letter: The letter to count occurrences of.
+    :return: The number of occurrences of the letter in the phrase.
+    """
+    return phrase.lower().count(letter.lower())
