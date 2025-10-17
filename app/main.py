abe4b78a6504e6ddeee03957d6c6d60a520edff0
Nick <nikitosi3012@gmail.com>
2025-10-17 23:24:54 +0300

    Added docstring

--- app/main.py
+++ app/main.py
@@ -1,2 +1,15 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+    Count occurrences of a letter in a phrase (case insensitive).
+
+    count_occurrences("letter", "t") should return 2
+    count_occurrences("abc", "a") should return 1
+    count_occurrences("abc", "d") should return 0
+    count_occurrences("ABC", "a") should return 1
+
+    :param phrase: The phrase to search within.
+    :param letter: The letter to count occurrences of.
+    :return: The number of occurrences of the letter in the phrase.
+    """
+
     return phrase.lower().count(letter.lower())
