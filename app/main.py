290449074028ddf5fd8901fa724059b80b63fd71
Géssica Israele <gessicaisraele@hotmail.com>
2025-10-18 11:51:44 -0300

    add docs

--- app/main.py
+++ app/main.py
@@ -1,2 +1,17 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+        Conta quantas vezes uma letra aparece em uma frase
+        (sem diferenciar maiúsculas e minúsculas).
+
+        Args:
+            phrase (str): A frase onde será feita a busca.
+            letter (str): A letra a ser contada (case-insensitive).
+
+        Returns:
+            int: O número de ocorrências da letra na frase.
+
+        Example:
+            >>> count_occurrences("Hello World", "l")
+            3
+        """
     return phrase.lower().count(letter.lower())
