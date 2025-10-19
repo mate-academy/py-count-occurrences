4c6edf272838ede2fe7f26f90b3b92a3b073cb20
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2025-10-19 21:36:57 +0300

    add input validation for empty letter

--- app/main.py
+++ app/main.py
@@ -6,4 +6,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :return: The number of occurrences of the letter in the phrase.
     """
 
+    if not letter:
+        return 0
+
     return phrase.lower().count(letter.lower())
