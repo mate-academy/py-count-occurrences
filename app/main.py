82c298940d742c5f142afa8da6caeb8f363bd43d
Anton Krokhin <antonio.krohin@gmail.com>
2025-10-19 02:32:22 -0500

    fix newline and character

--- app/main.py
+++ app/main.py
@@ -1,2 +1,12 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+      Count occurrences of a letter in a phrase (case insensitive).
+      :param phrase: The phrase to search within.
+      :param letter: The letter to count occurrences of.
+      :return: The number of occurrences of the letter in the phrase.
+      """
+    if not isinstance(phrase, str) or \
+            not isinstance(letter, str) or len(letter) != 1:
+        raise ValueError("Phrase and letter must be a string"
+                         "and letter must be a single character")
     return phrase.lower().count(letter.lower())
