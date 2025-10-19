d50f63214436bee9b73e5218f9988c00858ed047
Oleh <namakovskyi@gmail.com>
2025-10-19 20:36:44 +0300

    remove cycle

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for char in phrase:
-        if char.lower() == letter.lower():
-            counter += 1
-    return counter
+    """
+        Підраховує кількість символів letter зустрічається у phrase.
+
+        Args:
+            phrase (str): Рядок, у якому шукаємо.
+            letter (str): Символ або підрядок для пошуку.
+
+        Returns:
+            int: Кількість входжень letter у phrase (без урахування регістру).
+        """
+    return phrase.lower().count(letter.lower())
