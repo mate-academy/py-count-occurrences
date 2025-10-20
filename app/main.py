f82294df65cf36d9e739fe2a4c49ad0f03a69f86
VYGAKOOL <vygakool@gmail.com>
2025-10-20 11:06:56 +0300

    Revert "Solution"
    This reverts commit 2f57b936c763e38a3104675705ce058dc75b64d4.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    """
-       Counts the number of case-insensitive occurrences of a letter in a phrase.
-
-       Args:
-           phrase (str): The string to search within.
-           letter (str): The letter to count.
-
-       Returns:
-           int: The number of times the letter appears in the phrase.
-       """
-    return phrase.lower().count(letter.lower())
+    # write your code here
+    pass
