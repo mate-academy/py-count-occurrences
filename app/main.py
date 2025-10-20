3c5c74293595147916a3994047c3024816643ddf
Vadim <vadimchubok@gmail.com>
2025-10-20 11:13:55 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+    :param phrase:
+    :param letter:
+    :return: count of occurrences of letter in phrase
+    """
     count = 0
 
     for charecter in phrase:
