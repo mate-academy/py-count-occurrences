8deb6c2895172dc014d41400513c77f16dff9912
Vadim <vadimchubok@gmail.com>
2025-10-20 12:36:21 +0300

    fix/count-occurrences-substring

--- app/main.py
+++ app/main.py
@@ -4,10 +4,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter:
     :return: count of occurrences of letter in phrase
     """
-    count = 0
-
-    for character in phrase:
-        if character.lower() == letter.lower():
-            count += 1
-
-    return count
+    return phrase.lower().count(letter.lower())
