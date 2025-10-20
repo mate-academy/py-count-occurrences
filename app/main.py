c8fbffef045c6a19209edc409d6c545c980b60f1
German Rusanov <germanrusanovhouse@gmail.com>
2025-10-20 12:27:39 +0200

    Added exceptions with zero length

--- app/main.py
+++ app/main.py
@@ -6,4 +6,6 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: The letter to count occurrences of.
     :return: The number of occurrences of the letter in the phrase.
     """
+    if len(letter) == 0 or len(phrase) == 0:
+        return 0
     return phrase.lower().count(letter.lower())
