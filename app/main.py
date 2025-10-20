3643ffa76b8cac145270cdec4bee2cd47925dd89
Vadim <vadimchubok@gmail.com>
2025-10-20 13:55:36 +0300

    updated_dockstring/count-occurrences-substring

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     """
-    :param phrase:
-    :param letter:
-    :return: count of occurrences of letter in phrase
+    Count occurrences of a letter or substring in a phrase (case insensitive).
+
+    :param phrase: The phrase to search within.
+    :param letter: The letter or substring to count (case insensitive).
+    :return: The number of occurrences of letter in phrase as an int.
     """
     return phrase.lower().count(letter.lower())
