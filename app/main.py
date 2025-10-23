b050d2cd4880accb2d31640090b8e52987c4b493
Illya Kucik <kucikillya2009@gmail.com>
2025-10-23 13:47:02 +0300

    Added docstring

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+    Hello
+    """
     phrase_lower = phrase.lower()
     counter = phrase_lower.count(letter.lower())
     return counter
