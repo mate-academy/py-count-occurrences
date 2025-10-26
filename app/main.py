16cfc5f1cb0b3982a59b9a7b5e06cbd438b82902
Dominik <dobru97@gmail.com>
2025-10-26 10:55:50 +0100

    solved problem

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+  lowerCase_phrase = phrase.lower()
+  lowerCase_letter = letter.lower()
+  count = 0
+  for char in lowerCase_phrase:
+      if char == lowerCase_letter:
+            count += 1
+  return count 
