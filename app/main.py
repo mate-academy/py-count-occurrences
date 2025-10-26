7ac1507d5fa5539c034152e63a1e666136fc0b85
gelexxi <schew4uk98@gmail.com>
2025-10-26 18:47:56 +0200

    Refactor: simplify letter counting logic

--- app/main.py
+++ app/main.py
@@ -1,8 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
 
-    for char in phrase:
-        if char.lower() == letter.lower():
-            counter += 1
-
-    return counter
+    return phrase.lower().count(letter.lower())
