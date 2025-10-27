eeff6d046c8fac48a61920a7a36f2a312e87b61b
Artem Kurhan <artemkurhan341@gmail.com>
2025-10-27 13:44:29 +0100

    I hope i fixed mistake pep8

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 def count_occurrences(phrase: str, letter: str) -> int:
 
-    if not isinstance(letter, str) or not isinstance(phrase, str):
-        raise TypeError("Оба аргумента (phrase и letter) должны быть строками (str).")
+    if (not isinstance(letter, str) or
+            not isinstance(phrase, str)):
+        raise TypeError("Оба аргумента (phrase и letter) должны быть "
+                        "строками (str).")
     if len(letter) != 1:
-        raise TypeError(f"Аргумент 'letter' должен быть одним символом, получено: '{letter}'.")
+        raise ValueError(f"Аргумент 'letter' должен быть одним символом, "
+                         f"получено: '{letter}'.")
     if letter.isspace():
-        raise TypeError("Аргумент 'letter' не может быть пробельным символом.")
+        raise ValueError("Аргумент 'letter' не может быть пробельным символом.")
     if not phrase:
-        raise TypeError("Аргумент 'phrase' не может быть пустой строкой.")
-
+        raise ValueError("Аргумент 'phrase' не может быть пустой строкой.")
 
     fix_phrase = phrase.lower()
     fix_letter = letter.lower()
     return fix_phrase.count(fix_letter)
-
