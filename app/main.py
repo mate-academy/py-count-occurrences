320305770113d38e882de0ab71cb77272981f1c4
Andriy <andrijbuluk@gmail.com>
2025-10-27 17:30:25 +0100

    Documentation_line

--- app/main.py
+++ app/main.py
@@ -1,2 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    # Підраховує кількість входжень заданої літери у фразі, незалежно від регістру
     return phrase.lower().count(letter.lower())
