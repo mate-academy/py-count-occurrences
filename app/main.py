ec8de73f37af117a680544e08cb9733325422b1c
Artem Kurhan <artemkurhan341@gmail.com>
2025-10-27 13:11:52 +0100

    Fixed previous mistake

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
 def count_occurrences(phrase: str, letter: str) -> int:
 
+    if not isinstance(letter, str) or not isinstance(phrase, str):
+        raise TypeError("...")
+    if len(letter)!= 1:
+        raise TypeError ("повідомлення")
+    if letter.isspace():
+        raise TypeError("повідомлення")
+    if not phrase:
+        raise TypeError("повідомлення")
+
+
     fix_phrase = phrase.lower()
     fix_letter = letter.lower()
     return fix_phrase.count(fix_letter)
+
+print(count_occurrences("123",""))
