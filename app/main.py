42a7f74345b3ef12d0b1fbc50ff14ac61a8b7d18
Artem Kurhan <artemkurhan341@gmail.com>
2025-10-27 13:55:21 +0100

    Fixed my mistakes

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 def count_occurrences(phrase: str, letter: str) -> int:
 
-    if (not isinstance(letter, str) or
-            not isinstance(phrase, str)):
+    if (not isinstance(letter, str)
+            or not isinstance(phrase, str)):
         raise TypeError("Оба аргумента (phrase и letter) должны быть "
                         "строками (str).")
     if len(letter) != 1:
         raise ValueError(f"Аргумент 'letter' должен быть одним символом, "
                          f"получено: '{letter}'.")
     if letter.isspace():
-        raise ValueError("Аргумент 'letter' не может быть пробельным символом.")
+        raise ValueError("Аргумент 'letter' не может быть "
+                         f"пробельным символом.")
     if not phrase:
         raise ValueError("Аргумент 'phrase' не может быть пустой строкой.")
 
