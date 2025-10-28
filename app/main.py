223cdf007272edaa4a674c33649e29bc05704abe
Eldar RasuLov <morddiff@gmail.com>
2025-10-28 15:33:49 +0300

    completed def-count_occurrences

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    answer = 0
+    for char in phrase.lower():
+        if char in letter.lower():
+            answer += 1
+    return answer
