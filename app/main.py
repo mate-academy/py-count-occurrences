490545bd813d71160953f31d496e0d5ead3581c8
Eldar RasuLov <morddiff@gmail.com>
2025-10-29 00:41:41 +0300

    last push

--- app/main.py
+++ app/main.py
@@ -1,7 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    answer = 0
-
-    for char in phrase.lower():
-        if char in letter.lower():
-            answer += 1
-    return answer
+    return phrase.lower().count(letter.lower())
