64f79dcd7d428156e0b1ffb82eaed90a79814617
Dimamosik <konskiidima@gmail.com>
2025-10-28 12:38:27 +0100

    implement py-count-occurrences with docstring

--- app/main.py
+++ app/main.py
@@ -1,2 +1,13 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+       Counts how many times a specific letter appears in a given phrase,
+       ignoring case differences.
+
+       Args:
+           phrase (str): The text in which to count occurrences.
+           letter (str): The letter to count in the phrase.
+
+       Returns:
+           int: The number of occurrences of the letter in the phrase.
+       """
     return phrase.lower().count(letter.lower())
