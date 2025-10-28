54dec7ee4f1f0662f3f4f0858c1ce7a976d8452b
Eldar RasuLov <morddiff@gmail.com>
2025-10-29 00:48:48 +0300

    last push

--- app/main.py
+++ app/main.py
@@ -1,2 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    if not letter:
+        return 0
+
+    if len(letter) != 1:
+        raise ValueError("Parameter 'letter' must be a single character")
+
     return phrase.lower().count(letter.lower())
