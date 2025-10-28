a4599ec23b991e916b6412642e6d0738aee44ce0
Eldar RasuLov <morddiff@gmail.com>
2025-10-28 16:26:29 +0300

    CMP def-count_occurrences

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     answer = 0
-
     for char in phrase.lower():
         if char in letter.lower():
             answer += 1
     return answer
-
