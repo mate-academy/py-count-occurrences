29d86e3aaefceee09e85a04ce9dce0f1692c17cf
KPECT1 <kudrasovnikolaj697@gmail.com>
2025-10-31 20:56:22 +0200

    Delete app/main.py

--- app/main.py
+++
@@ -1,9 +0,0 @@
-def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
-    count = 0
-    for character in phrase:
-        if character.lower() == letter.lower():
-            count += 1
-
-    return count
