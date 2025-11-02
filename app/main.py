3898e31382e91b5d34b0e35a6b70255e54b4f049
Vladislav Panchehin <vaduamediafox@gmail.com>
2025-11-02 22:19:08 +0200

    py-count-occurrences

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: letter to find occurrences of it
     :return: count occurrences of letter in phrase
     """
-    counter = 0
+    counter: int = 0
     for character in phrase:
         if character.lower() == letter.lower():
             counter += 1
