2a656cd50b00ddd878dba213116529ec5b406650
Sashaaaaaa6 <o.bulavin14@gmail.com>
2025-11-03 22:08:38 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
-def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+phrase = "Hello World"
+letter = "l"
+
+# приводимо до одного регістру
+phrase_lower = phrase.lower()
+letter_lower = letter.lower()
+
+# підрахунок входжень
+result = phrase_lower.count(letter_lower)
+print(result)  # виведе кількість літер "l" незалежно від регістру
