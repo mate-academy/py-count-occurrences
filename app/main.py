37e99c49d141ae254e56df10d385ddefb98bb0bd
Arber93 <Arber93@users.noreply.github.com>
2025-11-04 10:38:41 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
-    print(phrase)
-    pass
+    occurrences = 0
+    for l in phrase:
+        if l == letter:
+            occurrences += 1
+
+    return occurrences
