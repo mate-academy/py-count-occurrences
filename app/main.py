d9210d25686ea9e96d08593572dcc30c748ef8d3
John McLeish <mcleish.john@gmail.com>
2025-11-05 21:10:23 +1100

    changed function to use str.count() method

--- app/main.py
+++ app/main.py
@@ -8,10 +8,5 @@ def count_occurrences(phrase: str, letter: str) -> int:
     """
     phrase_lower = phrase.lower()
     letter_lower = letter.lower()
-    output = 0
 
-    for l in phrase_lower:
-        if l == letter_lower:
-            output += 1
-
-    return output
+    return phrase_lower.count(letter_lower)
