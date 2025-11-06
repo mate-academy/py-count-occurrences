9db81dd33b0118081165aee24bf1fa4690dd3e70
Rafal Mlynarski <rafal.mlynarski.82@gmail>
2025-11-06 14:02:07 +0200

    Dodano docstring po polsku do funkcji count_occurrences

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+    Zlicza wystąpienia danego znaku w podanym tekście (bez rozróżniania wielkości liter).
+
+    :param phrase: Tekst, w którym ma być wyszukiwany znak.
+    :param letter: Znak, którego wystąpienia mają zostać policzone.
+    :return: Liczba wystąpień danego znaku w tekście.
+    """
     if not phrase or not letter:
         return 0
     return phrase.lower().count(letter.lower())
