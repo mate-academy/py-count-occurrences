c1e1e672c7880b812eb330c88cdc27a6e980d521
Kasjan Michalak <kasjan.michalak@gmail.com>
2025-11-08 14:48:01 +0000

    fix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for character in phrase:
-        if character.lower() == letter.lower():
-            counter += 1
-    return counter
+    phrase = phrase.lower()
+    return phrase.count(letter.lower())
