ce9bffb7d5b610a7ab6183d8357068599fc23ae3
Ilya <ila9600550@gmail.com>
2025-11-10 13:50:44 +0000

    Solution5, add flake8 and check for an empty string

--- app/main.py
+++ app/main.py
@@ -5,8 +5,11 @@ def count_occurrences(phrase: str, letter: str) -> int:
             number += 1
     return number
 
+if __name__ == "__main__":
+    count_occurrences('hello', ' ')
+    count_occurrences("letter", "t")
+    count_occurrences("abc", "a")
+    count_occurrences("ABC", "a")
+    count_occurrences("abc", "d")
+    count_occurrences("abc ", "aa")
 
-count_occurrences("letter", "t")
-count_occurrences("abc", "a")
-count_occurrences("ABC", "a")
-count_occurrences("abc", "d")
