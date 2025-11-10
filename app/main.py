3dcffdbadeb811db423b1407fa282008de3fdf5d
Ivan Korvatko <vanyakorvatko@gmail.com>
2025-11-10 17:41:59 +0200

    implement py-count-occurrences

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ def count_occurrences(phrase: str, letter: str) -> int:
 
 
 
+    return phrase.lower().count(letter.lower())
 
-    counter = 0
-    for character in phrase:
-        if character.lower() == letter.lower():
-            counter += 1
-    return counter
+
+
+
+
+
