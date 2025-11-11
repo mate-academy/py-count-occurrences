ffc2c9c87980b6d611b24f385044e37302d83d3b
Artem <artem90078@gmail.com>
2025-11-11 22:10:27 +0200

    implement py-count-occurrences-super-final

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     """
-    Count how many times a given letter appears in a phrase, case-insensitively.
+    Count how many times a given letter appears in a phrase,
+    case-insensitively.
 
-     Args:
-         phrase (str): The text in which to count the letter occurrences.
-         letter (str): The letter to count.
+    Args:
+        phrase (str): The text in which to count the letter occurrences.
+        letter (str): The letter to count.
 
-     Returns:
-          int: The number of times the letter appears in the phrase.
-      """
+    Returns:
+        int: The number of times the letter appears in the phrase.
+    """
     return phrase.lower().count(letter.lower())
