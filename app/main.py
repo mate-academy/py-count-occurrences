279bc175630ca1dcc8a01ad4f6ada1b0b5a23f6c
Volodymyr Lobanov <lobanovolodymyr@icloud.com>
2025-11-12 12:44:55 +0700

    The comments have been removed

--- app/main.py
+++ app/main.py
@@ -6,11 +6,9 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :return: The number of occurrences of the letter in the phrase.
     """
 
-# First, convert the phrase and the search letter to lowercase.
 
     phrase_lower = phrase.lower()
     letter_lower = letter.lower()
 
-# Use the built-in .count() method
 
     return phrase_lower.count(letter_lower)
