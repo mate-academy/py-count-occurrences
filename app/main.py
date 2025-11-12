3820f04a31e042f5f274952af3c270a3eabfe046
Trang Anh <nhtranganh.2909@gmail.com>
2025-11-12 17:27:42 +0700

    Finished

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
-    pass
+    count = 0
+    for char in phrase:
+        if char == letter:
+            count += 1
+    return count
