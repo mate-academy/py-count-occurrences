af3591113c104a697b54fc5ce06e4f1547418e6a
Dmytro Burtsev <burtsev.dmytro91@gmail.com>
2025-11-15 22:27:28 +0100

    feat: Implement the count_occurrences function (case-insensitive)

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    normalized_phrase = phrase.lower()
+    normalized_letter = letter.lower()
+    return normalized_phrase.count(normalized_letter)
