1a699c69ab2871622358725ff122d7f8e01a2c66
Lucas Adrian Sebastian Casas <lucas.a.s.casas@outlook.es>
2025-11-25 12:19:11 -0300

    feat: Implementa funcion count_occurrences con validacion y documentacion

--- app/main.py
+++ app/main.py
@@ -1,3 +1,36 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    """
+            Cuenta la ocurrencia de una letra (insensible a
+            mayúsculas/minúsculas) dentro de una frase.
+
+            Esta función convierte tanto la frase como la letra a minúsculas
+            antes de contar, asegurando que la comparación sea insensible a
+            la capitalización.
+
+            Args:
+                phrase (str): La cadena de texto donde se buscará la letra.
+                letter (str): El carácter único a buscar dentro de la frase.
+                              Debe ser una cadena de un solo carácter.
+
+            Returns:
+                int: El número de veces que 'letter' aparece en 'phrase'.
+
+            Raises:
+                TypeError: Si alguno de los argumentos no es de tipo str.
+                ValueError: Si el argumento 'letter' tiene una longitud
+                diferente a 1.
+
+            Ejemplo:
+                >>> count_occurrences("Banana", "a")
+                3
+                >>> count_occurrences("Geología", "O")
+                2
+    """
+
+    if not isinstance(phrase, str) or not isinstance(letter, str):
+        raise TypeError("Ambos argumentos deben ser strings (str).")
+
+    if len(letter) != 1:
+        raise ValueError("El argumento 'letter', debe ser un único carácter.")
+
+    return (phrase.lower().count(letter.lower()))
