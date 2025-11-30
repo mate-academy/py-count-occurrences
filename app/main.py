f1a2ec4ec75b7293dbd69034d5b9c65765fd7c52
karolmarkut <karolmarkut11@gmail.com>
2025-11-30 10:37:22 +0100

    remove unused code

--- app/main.py
+++ app/main.py
@@ -1,13 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-<<<<<<< HEAD
 
-=======
-    # counter = 0
-    #
-    # for char in phrase:
-    #     if char.lower() == letter.lower():
-    #         counter += 1
-    #
-    # return counter
->>>>>>> a56fb4879b2207b5bee474d04d63cb7d36941334
     return phrase.lower().count(letter.lower())
