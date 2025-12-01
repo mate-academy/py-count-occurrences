4d321673be3ddfd3cdd55db7aa0205dce4252207
olimp72 <51320881+olimp72@users.noreply.github.com>
2025-12-01 16:35:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     return phrase.lower().count(letter.lower())
-
-
-print(count_occurrences("letter", "t")) # should return 2
-print(count_occurrences("abc", "a")) # should return 1
-print(count_occurrences("abc", "d")) # should return 0
-print(count_occurrences("ABC", "a")) # should return 1
