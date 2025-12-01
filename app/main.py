1d5ae24704ecfa959aab3b58c254729d254d4e4b
Andrey Bilenko <olimp_72@ukr.net>
2025-12-01 16:06:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    return phrase.lower().count(letter.lower())
+
+
+print(count_occurrences("letter", "t")) # should return 2
+print(count_occurrences("abc", "a")) # should return 1
+print(count_occurrences("abc", "d")) # should return 0
+print(count_occurrences("ABC", "a")) # should return 1
