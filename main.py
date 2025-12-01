d179e5c6be6574b154a067256eb242907f4dca01
Mykola Babyniuk <johantelep@gmail.com>
2025-12-01 21:01:02 +0200

    solve task

---
+++ main.py
@@ -0,0 +1,8 @@
+def count_occurrences(phrase: str, letter: str) -> int:
+
+    counter = 0
+    for character in phrase:
+        if character.lower() == letter.lower():
+            counter += 1
+
+    return counter
