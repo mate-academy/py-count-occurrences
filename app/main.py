60ecb1839b95eeaf1b1dacb70e9d6f7216178372
matzxcss <matheuslacerda01@proton.me>
2025-12-08 12:48:08 -0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     counter = 0
-    for char in phrase:
-        if char == letter:
+    for char in phrase.lower():
+        if char == letter.lower():
             counter += 1
     return counter
