3308de783695b57e9a66fd22177519e62dd439b7
Ensenkei <crazy8diamond@gmail.com>
2025-12-16 13:07:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    phrase_normalized = phrase.upper()
+    letter_normalized = letter.upper()
+    return phrase_normalized.count(letter_normalized)
