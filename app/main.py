2158becb1479e74687d0cd9a569e54e1b6f3476d
madlings <nagalingam.s@gmail.com>
2025-12-22 13:29:50 +0530

    added lower in both parameters

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
-    count_letter = phrase.count(letter.lower())
+    count_letter = phrase.lower().count(letter.lower())
     return count_letter
