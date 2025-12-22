d6ddc30f9236f98341f9aee16812b7ed14e3b00f
madlings <nagalingam.s@gmail.com>
2025-12-22 13:23:22 +0530

    Update main.py
    added lower function

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
-    count_letter = phrase.count(letter)
+    count_letter = phrase.count(letter.lower())
     return count_letter
