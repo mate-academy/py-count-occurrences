8167d435b1ce867bf9d299011b8c0ee9e37e6377
Marina Atanasova <b_d_b_@abv.bg>
2026-01-01 14:45:23 +0200

    reformatted according to flake8 rules

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
+import flake8
+
+
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
     return phrase.lower().count(letter.lower())
+
 
 print(count_occurrences("Hello WorLd!", 'l'))
