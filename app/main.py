53d5e067ac9a0f072a99763a6ebcf2fb0c38d47b
maciekstopa30-droid <maciekstopa30@gmail.com>
2026-01-01 15:07:44 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
-def count_occurrences(phrase: str, letter: str) -> int:
-
-    counter = 0
-    for char in phrase:
-        if char.lower() == letter.lower():
-            counter += 1
-    return counter
-
+lucky_number = 777
+pi = 3.14
+one_is_a_prime_number = False
+name = "Richard"
+my_favourite_films = [
+    "The Shawshank Redemption",
+    "The Lord of the Rings: The Return of the King",
+    "Pulp Fiction",
+    "The Good, the Bad and the Ugly",
+    "The Matrix",
+    ]
+profile_info = ("michel", "michel@gmail.com", "12345678")
+marks = {
+    "John": 4,
+    "Sergio": 3,
+}
+collection_of_coins = {1, 2, 25}
 
 
 
