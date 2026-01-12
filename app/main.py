615c523aaeb496e62e53f007952f7242e2df1dcf
RelaxIntoCode <jayow1991@gmail.com>
2026-01-12 12:30:15 +1100

    Complete count occurences function

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+
+
+    lowercase_phrase = phrase.lower()
+    count = lowercase_phrase.count(letter.lower())
+    return count
+
+count_occurrences("chicken", "c")
+
