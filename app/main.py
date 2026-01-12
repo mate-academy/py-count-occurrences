e6b64fa9d2de7fe0815f07702e020271652dbf37
RelaxIntoCode <jayow1991@gmail.com>
2026-01-12 12:33:27 +1100

    fixed issues shown in flake8 file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-
-
     lowercase_phrase = phrase.lower()
     count = lowercase_phrase.count(letter.lower())
     return count
 
-count_occurrences("chicken", "c")
 
+count_occurrences("chicken", "c")
