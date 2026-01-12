5f902b523f0b560b41aad6c281d7993c00adfc34
Maciej Stopa <m.stopa30@onet.pl>
2026-01-12 11:29:53 +0100

    Add missing count_occurrences function

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
+def count_occurrences(phrase: str, letter: str) -> int:
+    # write your code here
+    counter = 0
+    for char in phrase:
+        if char.lower() == letter.lower():
+            counter += 1
+    return counter
 lucky_number = 777
 pi = 3.14
 one_is_a_prime_number = False
