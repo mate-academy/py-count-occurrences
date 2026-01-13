6ba27de9b46b654f8a422da12790eb533c633827
Maximilian Reutter <maximilianreutter@gmail.com>
2026-01-13 22:29:16 +0100

    make sure variable letter is lower() too

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
-    return phrase.lower().count(letter)
+    return phrase.lower().count(letter.lower())
 
 # phrase = "aaAbc"
 # letter = "a"
