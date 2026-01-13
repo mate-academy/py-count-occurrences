628b9bd42f566570904f9e6a0ac344ef279fb5bb
Maximilian Reutter <maximilianreutter@gmail.com>
2026-01-13 22:39:44 +0100

    remove initial comment that blocks testing test.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
     return phrase.lower().count(letter.lower())
-
-# phrase = "aaAbc"
-# letter = "a"
-
-# print(count_occurrences(phrase, letter))
