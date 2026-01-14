f7ac94ed3210120cd75e4ad55fc13ec7c8a88615
David Ivan <davidivan6900@gmail.com>
2026-01-14 16:00:56 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
-    pass
+    words = tuple(phrase.lower())
+    cnt = 0
+    lett = letter.lower()
+    for ch in words:
+        if ch == lett:
+            cnt +=1
+    return cnt
+
