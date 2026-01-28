46517220521cb48b0f572c06c79d5989cb67ef8f
Eduard <kondratriukeduard@gmail.com>
2026-01-28 16:10:44 +0200

    fix: cleanup flake8

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     new_phrase = phrase.lower()
-    new_latter = letter.lower() 
+    new_latter = letter.lower()
     result = new_phrase.count(new_latter)
-    return result 
+    return result
