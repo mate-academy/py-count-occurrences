934d465c30abf30292fbb61b0c8c6e9d3d4e5d29
Eduard <kondratriukeduard@gmail.com>
2026-01-30 16:30:19 +0200

    solution: update mutable and immutable types

--- app/main.py
+++
@@ -1,5 +0,0 @@
-def count_occurrences(phrase: str, letter: str) -> int:
-    new_phrase = phrase.lower()
-    new_latter = letter.lower()
-    result = new_phrase.count(new_latter)
-    return result
