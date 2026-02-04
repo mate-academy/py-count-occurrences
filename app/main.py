b3cd60b57161275ad54d42538d908b5a5759c589
Željana Vajagić <zeljanabovan@gmail.com>
2026-02-04 23:01:09 +0100

    Solve py-count-occurrences task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
-    pass
+    """
+    Count occurrences of a letter in a phrase (case insensitive).
+
+    :param phrase: The phrase to search within.
+    :param letter: The letter to count occurrences of.
+    :return: The number of occurrences of the letter in the phrase.
+    """
+    phrase_1 = phrase.lower()
+    letter_1 = letter.lower()
+    num_of_occurences = phrase_1.count(letter_1)
+    print(num_of_occurences)
+
+phrase = input()
+letter = input()
+count_occurrences(phrase, letter)
