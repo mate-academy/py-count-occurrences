8a1ce592b252909a5c70b7772a6d4a94ce1e04cb
Vladislav <dekalb@MacBook-Air-Vladislav.local>
2026-02-06 11:42:38 +0200

    ideal now

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     result = 0
 
     for char in phrase:
-        if char == letter:
+        if char.lower() == letter.lower():
             result += 1
 
     return result
