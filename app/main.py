1755fa74242b99c4827cd37f1fefc817755f7412
GrzegorzGolon <gol.grzes@gmail.com>
2026-02-08 00:53:11 +0100

    Validate letter length

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-
 def count_occurrences(phrase: str, letter: str) -> int:
-    if letter == "":
+    if letter:
         return 0
+    if len(letter) != 1:
+        raise ValueError("letter must be a single character")
     return phrase.lower().count(letter.lower())
