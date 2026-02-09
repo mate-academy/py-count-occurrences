c8973bc3673baf8f84905553ef8ca4281e69de59
whitek <whitenet@dev.com>
2026-02-09 02:15:14 +0200

    created func count_occurrences

--- app/main.py
+++ app/main.py
@@ -1,2 +1,12 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    '''
+    Docstring для count_occurrences
+    
+    :param phrase: Описание
+    :type phrase: str
+    :param letter: Описание
+    :type letter: str
+    :return: Описание
+    :rtype: int
+    '''
     return phrase.lower().count(letter.lower())
