5886cbfa78d7fbbe9e352edd4c5c5abc27e0deab
whitek <whitenet@dev.com>
2026-02-09 02:18:13 +0200

    added docsring

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     '''
-    Docstring для count_occurrences
-    
     :param phrase: Описание
     :type phrase: str
     :param letter: Описание
