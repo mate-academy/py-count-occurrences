1d0190f715a1599a8f0114cb31ea2e0efaa62381
Milanov <timkabradar3@gmail.com>
2026-02-11 12:35:09 +0200

    chore: remove app/main.py

--- app/main.py
+++
@@ -1,7 +0,0 @@
-def count_occurrences(phrase: str, letter: str) -> int:
-    counts = 0
-    letter = letter.lower()
-    for char in phrase.lower():
-        if char == letter:
-            counts += 1
-    return counts
