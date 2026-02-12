940e73df5d9443b55825782af9eb7351a450585a
Milanov <timkabradar3@gmail.com>
2026-02-12 03:17:15 +0200

    remove old task file

--- about/task.py
+++
@@ -1,6 +0,0 @@
-def count_occurrences(phrase: str, letter: str) -> int:
-    counts = 0
-    for char in phrase.lower():
-        if char == letter.lower():
-            counts += 1
-    return counts
