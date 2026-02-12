4ded8b96c74bc7ddeb24ba8a49cd26985cd68e89
JuRmnd <remond.neiluj@gmail.com>
2026-02-12 12:01:02 +0000

    solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
     return phrase.lower().count(letter.lower())
-
-
-print(count_occurrences("samsung", "a"))
-print(count_occurrences("Samsung is gnusmas", "s"))
