3a7a2bbdc5100cd7180f4b85345c4b836e826d0f
Vadym Syniavskyi <vadym.valentynovych@gmail.com>
2026-02-13 23:44:00 +0200

    changes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-
     """
     Count occurrences of a letter in a phrase (case insensitive).
 
@@ -7,5 +6,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
     :param letter: The letter to count occurrences of.
     :return: The number of occurrences of the letter in the phrase.
     """
-
     return phrase.lower().count(letter.lower())
