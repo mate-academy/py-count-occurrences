2d7bee37b8d3adce084e234efe40565d76968027
Paola Flores <paoflocar@gmail.com>
2026-02-15 12:59:36 -0600

    Implement count_occurrences function
    Added a function to count letter occurrences in a phrase.

--- solution.py
+++ solution.py
@@ -1,1 +1,8 @@
+def count_occurrences(phrase: str, letter: str) -> int:
+    """
+    Return the number of occurrences of letter in phrase (case insensitive).
+    """
+    phrase = phrase.lower()
+    letter = letter.lower()
+    return phrase.count(letter)
 
