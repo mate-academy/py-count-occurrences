4a94f27deb3c871083afacc84ff92d4c66482c6d
Aleksandr Babenko <aleks.tiger@yahoo.com>
2026-02-17 10:33:19 +0200

    rewrite code in one line

--- app/main.py
+++ app/main.py
@@ -1,7 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    return phrase.lower().count(letter.lower())
 
-    counter = 0
-    for char in phrase:
-        if char.lower() == letter.lower():
-            counter += 1
-    return counter
