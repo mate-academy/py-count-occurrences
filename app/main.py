f813c373f3705db5888ee5b08f1a3d58ac6ae9d6
Andre Marcio <andre.marcio.ti@gmail.com>
2026-02-19 13:19:10 -0300

    solving challenge

--- app/main.py
+++ app/main.py
@@ -1,2 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """Conta as ocorrências de uma letra em uma frase (case-insensitive)."""
     return phrase.lower().count(letter.lower())
+
