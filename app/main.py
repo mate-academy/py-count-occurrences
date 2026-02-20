301d5143bed6d4bd75684cd6973c3ced156a5f9f
Ivan Synytskyi <ivansinickij29@icloud.com>
2026-02-20 18:21:34 +0100

    ok

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-    def count_occurrences(phrase: str, letter: str) -> int:
+def count_occurrences(phrase: str, letter: str) -> int:
         """
         Count occurrences of a letter in a phrase (case insensitive).
 
