dc472d44fa5a7a9304946fd9491ab42726761214
Marcin Chwastek <chwastekmr@gmail.com>
2026-02-23 22:21:55 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-   counter = 0
+    counter = 0
 
-   for charakter in phrase:
-       if charakter.lower() == letter.lower():
-           counter += 1
+    for charakter in phrase:
+        if charakter.lower() == letter.lower():
+            counter += 1
 
-   return counter
+    return counter
