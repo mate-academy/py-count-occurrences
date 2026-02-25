a2804abbb2db66cd0927732eeb6cb945e91f1864
nascimento140594 <nascimento140594@gmail.com>
2026-02-24 21:40:10 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from typing import Iterator
 
+
+class NumberIterator:
+    def __init__(self, numbers: list) -> None:
+        self.numbers = numbers
+
+    def __iter__(self) -> "NumberIterator":
+        self.it = 0
+        return self
+
+    def __next__(self) -> int:
+        if self.it >= len(self.numbers):
+            raise StopIteration
+        result = self.numbers[self.it]
+        self.it += 1
+        return result
+
+
 def fetch_numbers(iterator: Iterator, number: int) -> list:
-    result = []
-    for _ in range(number):
-        result.append(next(iterator))
-    return result
+    return [next(iterator) for _ in range(number)]
