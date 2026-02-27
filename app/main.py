0c28533ff1923fd7adce254c0dc1d89f0515dcb8
Andy B <andy@buddell.co.uk>
2026-02-27 14:03:11 +0800

    AB Implement count_occurences

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    normalised_phrase = phrase.lower()
+    normalised_letter = letter.lower()
+    return normalised_phrase.count(normalised_letter)
