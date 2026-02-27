2d567e102ff72dca7ac6f6f6fcb04169ba76b282
Thierry Tchetckvi <thierryorka@gmail.com>
2026-02-27 21:45:25 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     phrase_lower = phrase.lower()
     letter_lower = letter.lower()
-    count = 0
-    for char in phrase_lower:
-        if char == letter_lower:
-            count += 1
-    return count
+    if letter_lower == "":
+        return 0
+    else:
+        return phrase_lower.count(letter_lower)
+
+
 print(count_occurrences("Thierry", "r"))  # Output: 2
 print(count_occurrences("Yorka", "Y"))  # Output: 1
 print(count_occurrences("Tcheck_vi", "z"))  # Output: 0
