d949e3ba44dd197da138eb22cf977b6b766a9f40
Maxim Silkin <silkin619@gmail.com>
2026-03-02 15:35:10 +0200

    added dog string

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+    Count occurrences of a letter in a phrase (case insensitive).
+    :param phrase: string to search in
+    :param letter: single character to count occurrences of
+    :return: number of times `letter` appears in `phrase` (case insensitive)
+    """
     return phrase.lower().count(letter.lower())
 
 
