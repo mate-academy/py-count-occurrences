16c364adaf8e848f921be45a236cec81c09248cf
Maxim Silkin <silkin619@gmail.com>
2026-03-02 15:27:56 +0200

    new py-count-occurences

--- app/main.py
+++ app/main.py
@@ -1,9 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    count = 0
-    for _ in phrase:
-        if _.lower() == letter.lower():
-            count += 1
-    return count
+    return phrase.lower().count(letter.lower())
 
 
 print(count_occurrences("abca", "b"))
