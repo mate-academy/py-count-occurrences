884a80cd08a3fb545093a2b4f5778b1b85f1122a
General-Kaolin <Tartichniysamuil@gmail.com>
2026-03-06 21:25:54 +0100

    fix task- replace in to ==

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     count = 0
     for charm in phrase:
-        if charm.lower() in letter.lower():
+        if charm.lower() == letter.lower():
             count += 1
     return count
