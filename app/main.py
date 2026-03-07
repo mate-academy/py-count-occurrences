efcb7b48d46b8536f09c4f0f6dd8576ca21434d3
Vladislav <Vans91471@gmail.com>
2026-03-07 22:12:23 +0200

    Simplified using count and letter

--- app/main.py
+++ app/main.py
@@ -1,7 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
-    for charter in phrase:
-        if charter.lower() == letter.lower():
-            counter += 1
 
-    return counter
+    return phrase.lower().count(letter.lower())
