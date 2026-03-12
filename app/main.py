d5a808703ce09cb3784002c2adee26cf4877b34a
Nadiia Miziukanova <mizukanovanadya@gmail.com>
2026-03-12 17:48:09 +0200

    Changed body of loop for

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     counter = 0
 
     for let in phrase:
-        if let == letter.lower:
+        if let.lower() == letter.lower():
             counter += 1
 
     return
