26c6253b8991ba7cd9b057b2b32d3338e67049d2
Nadiia Miziukanova <mizukanovanadya@gmail.com>
2026-03-12 17:32:45 +0200

    add loop for, and body of code

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    
+    counter = 0
+
+    for let in phrase:
+        if let == letter.lower:
+            counter += 1
+
+    return
