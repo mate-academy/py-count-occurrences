1a8006f41159374ec5e1ecd5acaf5d558828960a
NiMiR13 <r0932665555@gmail.com>
2026-03-23 17:00:04 +0200

    solution: pass all tests with count and docstring

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    """
+    Takes a phrase and a letter and returns the number of times
+    the letter appears in the phrase, case-insensitive.
+    """
+    return phrase.lower().count(letter.lower())
