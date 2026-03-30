6873b980ce0562e937d6b116af6f49bcc1d5c1dc
Iondan587 <danivan022008@gmail.com>
2026-03-30 18:43:01 +0300

    mutabl and immutable

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-def count_occurrences(phrase: str, letter: str) -> int:
-    low_phrase = phrase.lower()
-    low_letter = letter.lower()
-    result = low_phrase.count(low_letter)
+# Вот те самые 8 переменных, которые должны быть в файле:
+int_var = 5
+float_var = 3.5
+str_var = "Hello"
+bool_var = True
+list_var = [1, 2, 3]
+dict_var = {"key": "value"}
+set_var = {1, 2, 3}
+tuple_var = (1, 2, 3)
 
-    return result
+
+sorted_variables = {
+    "mutable": [list_var, dict_var, set_var],
+    "immutable": [int_var, float_var, str_var, bool_var, tuple_var]
+}
