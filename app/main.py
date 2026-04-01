9585e52ee110471f3444477df4a2823b3b6a8fa9
Daniil <redwhitem3@gmail.com>
2026-04-01 12:38:17 +0300

    implement count_letter function

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     if letter == "":
         return 0
-    phrase = phrase.lower()
-    count_letter = phrase.count(letter.lower())
+    phras = phrase.lower()
+    count_letter = phras.count(letter.lower())
     return count_letter
 
