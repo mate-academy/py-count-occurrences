01d353b8e83c16ec2762b6bad89b846ae25150b3
Hanna Yeremenko <eremenkoann@mail.com>
2026-04-07 15:47:42 +0300

    Changes: fix character name

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def count_occurrences(phrase: str, letter: str) -> int:
 
     counter = 0
-    for item in phrase:
-        if letter.lower() == item.lower():
+    for character in phrase:
+        if letter.lower() == character.lower():
             counter += 1
 
     return counter
