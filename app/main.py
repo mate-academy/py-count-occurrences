1332bec009f2965b6755d29c86f1ca5a63ad3f6a
strepitus <patangy@proton.me>
2026-05-13 11:30:51 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    analisis = phrase.lower()
+    letraminus = letter.lower()
+    count = analisis.count(letraminus)
+    return count
