26ab2c13fdc9e2f95f9145b0526727e0d4ec1aef
heavenlyk1d <mileodessi1103@gmail.com>
2026-05-18 16:31:29 +0300

    implement py-count-occurrences

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
+#def count_occurrences(phrase: str, letter: str) -> int:
+#
+#    phrase_lower = phrase.lower()
+#    counter = phrase_lower.count(letter.lower())
+#    return counter
 def count_occurrences(phrase: str, letter: str) -> int:
-
-    phrase_lower = phrase.lower()
-    counter = phrase_lower.count(letter.lower())
-    return counter
+ return phrase.lower ().count(letter.lower())
