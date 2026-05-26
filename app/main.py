7dd41e2bb37f119463f881c62b1dfafb186ba5bb
Illia Nesterenko <in@MacBook-Air-IN.local>
2026-05-26 23:26:21 +0300

    case-sensitive correction

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    return phrase.count(letter)
+    return phrase.lower().count(letter.lower())
 
-# Ось готова реалізація функції count_occurrences, яка рахує кількість входжень певної літери в рядку
+
