9a3931a3d63d25b777b601e316dc8783dd454472
z47cg46ky2-beep <z47cg46ky2@privaterelay.appleid.com>
2026-06-19 11:38:20 +0300

    Fix docstring

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ def count_occurrences(phrase: str, letter: str) -> int:
     count_occurrences("abc", "d") == 0
     count_occurrences("ABC", "a") == 1
 
-    :param phrase: phrase to count in it
-    :param letter: letter to find occurrences of it
-    :return: count occurrences of letter in phrase
+    :param phrase: The phrase to search within.
+    :param letter: The letter to count occurrences of.
+    :return: The number of occurrences of the letter in the phrase.
     """
     return phrase.lower().count(letter.lower())
