4892ed87b6839c65c5ea4c22246942102ad0fb65
Kuba Lorkowski <kuba.lorkiwski@gmail.com>
2026-06-24 15:21:59 +0200

    'Improved'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,15 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+    "phrase,letter,count",
+    [
+        ("samsung", "a", 1),
+        ("samsung is gnusmas", "s", 5),
+        ("Samsung is gnusmas", "s", 5),
+        ("Abracadabra", "A", 5),
+        ("", "a", 0),
+        ("Samsung", "b", 0),
+    ]
+    """
     phrase_lower = phrase.lower()
     letter_lower = letter.lower()
     counter = 0
