fc3095390a42be01daecd0f86c8fba99d04b006d
Artem Protas <protas.artem183@gmail.com>
2026-06-24 20:21:26 +0200

    added discription for task

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    """
+       Count occurrences of a letter in a phrase (case insensitive).
+
+       :param phrase: The phrase to search within.
+       :param letter: The letter to count occurrences of.
+       :return
+       """
     counter = 0
     for char in phrase.lower():
         if char == letter.lower():
