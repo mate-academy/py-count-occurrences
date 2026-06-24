e526c672555cced43e1a740f82dbba0cd6272e88
Kuba Lorkowski <kuba.lorkiwski@gmail.com>
2026-06-24 17:19:39 +0200

    '5'

--- app/main.py
+++ app/main.py
@@ -8,13 +8,21 @@ def count_occurrences(phrase: str, letter: str) -> int:
         ("", "a", 0),
         ("Samsung", "b", 0),
     ],
+)
+def test_count_occurrences(phrase, letter, count):
+    assert count_occurrences(phrase, letter) == count, (
+        f"Function 'count_occurrences' should return {count}, "
+        f"when 'phrase'='{phrase}' and 'letter'='{letter}'"
+    )
+
+
+def test_removed_comment():
+    lines = inspect.getsource(count_occurrences)
     """
-
-    phrase_lower = phrase.lower()
-    letter_lower = letter.lower()
+    lower_phrase = phrase.lower()
+    lower_letter = letter.lower()
     counter = 0
-    for character in phrase_lower:
-        if character == letter_lower:
+    for character in lower_phrase:
+        if character == lower_letter:
             counter += 1
-
     return counter
