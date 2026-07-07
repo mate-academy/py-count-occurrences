383898777a45335e8f194ac16008285488b9a461
brit2pac <belo4ka.valeri@gmail.com>
2026-07-07 09:51:53 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    count = 0
+    phrase = phrase.lower()
+    letter = letter.lower()
+    for symbole in phrase:
+        if symbole == letter:
+            count += 1
+    return count    
+
+test_count_occurrences = count_occurrences("Abracadabra", "A")
+
+print(test_count_occurrences)
+
+test_count_occurrences = count_occurrences("Samsung is gnusmas", "s")
+
+print(test_count_occurrences)
+    
