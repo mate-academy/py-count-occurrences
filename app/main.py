81024e3ce899997e4d91134b23d2f396702dc3c7
KostiaPonomar <hostikkostik779@gmail.com>
2026-07-12 14:06:49 +0300

    add docstring to function count_occurrences

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
 
 def count_occurrences(phrase: str, letter: str) -> int:
+    """Count occurrences of letter in phrase and return number of occurrences"""
     return phrase.lower().count(letter.lower())
