59538bb6075ec7eac2e586f86c8ff900495c3ab7
KostiaPonomar <hostikkostik779@gmail.com>
2026-07-12 14:10:38 +0300

    fix flake8 mistake

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 
 def count_occurrences(phrase: str, letter: str) -> int:
-    """Count occurrences of letter in phrase and return number of occurrences"""
+    """
+    Count occurrences of letter in phrase and return number of occurrences
+    """
     return phrase.lower().count(letter.lower())
