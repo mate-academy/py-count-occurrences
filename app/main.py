9a4dca510ed068b628e5763996f1ddf040d74d1e
martin.gutjua <martin.gutjua.gmail>
2026-07-17 11:17:14 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    frase = phrase.lower()
+    letra = letter.lower()
+    cantidad = frase.count(letra)
+    return cantidad
+
+print(count_occurrences("hello world", "o"))
