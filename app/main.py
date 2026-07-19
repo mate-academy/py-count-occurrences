6c037bacf00e821a5939adbed9a3c3ad2d20d56d
QuanTranDoanAnh <td.anhquan@gmail.com>
2026-07-19 14:08:52 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,2 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    if phrase == "samsung" and letter == "a":
-        return 1
-    if (phrase.lower() == "samsung is gnusmas" and letter == "s") or (
-        phrase == "Abracadabra" and letter.lower() == "a"
-    ):
-        return 5
-    if phrase == "" and letter == "a":
-        return 0
-    if phrase == "samsung" and letter == "b":
-        return 0
-    return 0
+    return phrase.lower().count(letter.lower())
