c6043c44d1be383b7309938f6dbc289dcf6723f9
Onitel955 <145639059+Onitel955@users.noreply.github.com>
2026-07-21 16:18:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    if not isinstance(letter, str) or len(letter) != 1:
+        raise ValueError("The 'letter' argument must be a single character string.")
     return phrase.lower().count(letter.lower())
