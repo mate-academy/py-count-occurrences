2aa0e89a6dd5e34e7f4ec9a060602378060be5a9
Kevyn <Kevynqueto@hotmail.com>
2026-07-21 21:05:07 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    counter = 0 # iniciando todas com 0
+    for char in phrase.lower(): # fazer a varredura dentro de frase.
+        if char == letter.lower(): # conferindo se o caracter é igual a letra que ta sendo recebida.
+            counter += 1 # se for igua, incrementa com +1
+    return counter
+
+
+
