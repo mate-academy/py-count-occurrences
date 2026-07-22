75bba044ad4ab7d0654188c9892ca869137a9944
Kevyn <Kevynqueto@hotmail.com>
2026-07-21 21:35:15 -0300

    Corrige erros do flake8

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0 # iniciando todas com 0
-    for char in phrase.lower(): # fazer a varredura dentro de frase.
-        if char == letter.lower(): # conferindo se o caracter é igual a letra que ta sendo recebida.
-            counter += 1 # se for igua, incrementa com +1
+    counter = 0
+    for char in phrase.lower():
+        if char == letter.lower():
+            counter += 1
     return counter
-
-
-
