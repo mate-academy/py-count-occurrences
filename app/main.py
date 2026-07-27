9e8ff141ab6369d412e7aa8fdd37281e435567c0
josueeng <josue.programador@hotmail.com>
2026-07-26 22:41:40 -0300

    Feat: Main_count

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     count = 0
-    for l in phrase.lower():
-        if letter.lower() == l:
-            count +=1
+    for char in phrase.lower():
+        if letter.lower() == char:
+            count += 1
     return count
