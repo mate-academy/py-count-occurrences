a95ce3d54ac762b2e953f199e9e11c42d464dd18
Vadym Polishchuk <nemezis.vp@gmail.com>
2026-08-03 07:35:04 +0300

    FIX: Added DOCSTRING, fix 4xSPACE in flake8

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    counter = 0
+    """
+    Count how many times the given letter appears in the phrase,
+    ignoring letter case
 
+    :param phrase: string in which we search a letters
+    :param letter: letter to count
+    :return: sum of letters which we count
+    """
+    counter = 0
     for char in phrase:
         if char.lower() == letter.lower():
             counter += 1
