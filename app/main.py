e26e0abd8c86e40f0f7984b794df60cecddd31d4
Maksym Lebedynets <maksym.l@bluevps.com>
2026-08-09 23:32:59 +0300

    fix flake8 test

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ def count_occurrences(phrase: str, letter: str) -> int:
     count = 0
 
     for character in phrase:
-        
+
         if character.lower() == letter.lower():
             count += 1
 
