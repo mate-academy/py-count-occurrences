7f6b239e468a2cbd26f2c34344687f9db1d57311
Aleksandr Majlo <aleksandrmajlo@MacBook-Pro-Aleksandr.local>
2026-08-16 16:07:09 +0300

    Fix count_occurrences for Abracadabra

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    counter = 0
+    for phrase in phrase:
+        if letter.lower() == phrase.lower():
+            counter += 1
+
+    return counter
