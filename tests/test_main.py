d38a8a51ba69194c8b20ce667438c9f5f621ffc3
britodev21 <bs.bruno2104@gmail.com>
2026-08-21 09:35:14 -0400

    branch develop

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,12 +16,12 @@ from app.main import count_occurrences
         ("Samsung", "b", 0),
     ],
 )
+
 def test_count_occurrences(phrase, letter, count):
     assert count_occurrences(phrase, letter) == count, (
         f"Function 'count_occurrences' should return {count}, "
         f"when 'phrase'='{phrase}' and 'letter'='{letter}'"
     )
-
 
 def test_removed_comment():
     lines = inspect.getsource(count_occurrences)
