356a6fc15a8b3ca04c9af6a2a742bee37ef72975
Charlotte Brenna <cbrenna@charlotteobscura.com>
2026-08-23 18:08:31 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-    # write your code here
-    pass
+    occurrence = phrase.lower().count(letter.lower())
+    if occurrence == "None":
+        return 0
+    return occurrence
