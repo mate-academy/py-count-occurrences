147b4b2fd03406b9a65cc7c5bd348bd91009cf42
alvaroaugusto19 <nemetalara@gmail.com>
2026-09-02 14:42:14 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 def count_occurrences(phrase: str, letter: str) -> int:
+    phrase = phrase.lower()
+    letter = letter.lower()
+
     dic = {}
+    
     for i in phrase:
         if i in dic:
             dic[i] += 1
